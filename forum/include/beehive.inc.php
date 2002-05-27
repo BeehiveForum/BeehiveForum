056b7619869daee236a3401306f462bfbf2f6ef7
Mark Rendle <no-reply@beehiveforum.co.uk>
2002-05-27 16:31:17 +0000

    Added Files: beehiveforum/forum/include/beehive.inc.php

---
+++ forum/include/beehive.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+/*======================================================================
+Copyright Project BeehiveForum 2002
+
+This file is part of BeehiveForum.
+
+BeehiveForum is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BeehiveForum is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Beehive; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+USA
+======================================================================*/
+
+function draw_beehive_bar()
+{
+    echo "<table width=\"96%\" class=\"posthead\"><tr>\n";
+    echo "<td align=\"right\" class=\"smalltext\">\n&copy;2002 ";
+    echo "<a href=\"http://beehiveforum.sourceforge.net/\" target=\"_blank\">";
+    echo "Project BeehiveForum</a></td></tr></table>";
+}

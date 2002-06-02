5722c55b6e8bca443f425fabd679ce20b0b80141
HeyRatFans <matt@heyratfans.co.uk>
2002-06-02 19:34:02 +0000

    Miffle!

--- forum/include/config.inc.php
+++ forum/include/config.inc.php
@@ -26,9 +26,9 @@ USA
 // Database stuff ------------------------------------------------------
 
 $db_server	= "localhost";	// the address of your MySQL server
-$db_username = "bigbeige";	// your MySQL username
-$db_password = "9826442";	// your MySQL password
-$db_database = "bigbeige_beehive";	// the name of your MySQL database
+$db_username = "beehiveforum";	// your MySQL username
+$db_password = "miffle";	// your MySQL password
+$db_database = "beehiveforum";	// the name of your MySQL database
 
 // ---------------------------------------------------------------------
 

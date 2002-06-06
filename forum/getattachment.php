01b6a4742bc9636965bdfedefdcc867233c5f9ff
HeyRatFans <matt@heyratfans.co.uk>
2002-06-06 17:38:16 +0000

    A few attachment bugs

--- forum/getattachment.php
+++ forum/getattachment.php
@@ -21,31 +21,56 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-require_once("./include/attachments.inc.php");
 require_once("./include/header.inc.php");
+require_once("./include/session.inc.php");
+
+if(!bh_session_check()){
+
+    $uri = "http://".$HTTP_SERVER_VARS['HTTP_HOST'];
+    $uri.= dirname($HTTP_SERVER_VARS['PHP_SELF']);
+    $uri.= "/logon.php?final_uri=";
+    $uri.= urlencode($HTTP_SERVER_VARS['REQUEST_URI']);
+    
+    header_redirect($uri);
+}
+
 require_once("./include/html.inc.php");
+require_once("./include/attachments.inc.php");
+require_once("./include/user.inc.php");
 
 if (isset($HTTP_GET_VARS['owneruid']) && isset($HTTP_GET_VARS['filename'])) {
 
   $userinfo = user_get($HTTP_GET_VARS['owneruid']);
-  $attachments_dir = "http://". $HTTP_SERVER_VARS['HTTP_HOST']. dirname($HTTP_SERVER_VARS['PHP_SELF']). '/attachments/'. $userinfo['LOGON'];
-  header_redirect($attachments_dir. '/'. $HTTP_GET_VARS['filename']);
-  exit;
   
-}else {
+  if (isset($userinfo['LOGON'])) {
+  
+    $attachments_dir = dirname($HTTP_SERVER_VARS['SCRIPT_FILENAME']). '/attachments/'. $userinfo['LOGON'];
+  
+    if (file_exists($attachments_dir. '/'. $HTTP_GET_VARS['filename'])) {
 
-  if (isset($HTTP_GET_VARS['msg'])) {
+      if(isset($HTTP_GET_VARS['download'])) {
   
-    header_redirect("messages.php?msg=". $HTTP_GET_VARS['msg']);
-    exit;
+        header("Content-Type: application/x-ms-download");
+        header("Content-Length: ". filesize($attachments_dir. '/'. $filename));
+        header("Content-disposition: filename=". $filename);
+        header("Content-Transfer-Encoding: binary");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+
+        readfile($attachments_dir. '/'. $HTTP_GET_VARS['filename']);
+        exit;
+      
+      }else {
+  
+        $attachments_dir = "http://". $HTTP_SERVER_VARS['HTTP_HOST']. dirname($HTTP_SERVER_VARS['PHP_SELF']). '/attachments/'. $userinfo['LOGON'];
+        header_redirect($attachments_dir. '/'. $HTTP_GET_VARS['filename']);
+    
+      }
+    
+    }
     
   }
   
 }
-  
-html_draw_top();
-echo "<h2>There was a problem downloading the attachment.</h2>";
-html_draw_bottom();
-exit;
 
 ?>

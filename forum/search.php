30f940db61ad9e0e8e78cce58bc719c3b00a2efd
Matt Beale <matt@beehiveforum.co.uk>
2002-06-08 21:11:13 +0000

    Search engine seems to work quite well, y'know

--- forum/search.php
+++ forum/search.php
@@ -35,32 +35,121 @@ if(!bh_session_check()){
     header_redirect($uri);
 }
 
-//require_once("./include/search.inc.php");
+require_once("./include/search.inc.php");
 require_once("./include/html.inc.php");
 require_once("./include/form.inc.php");
 require_once("./include/folder.inc.php");
-require_once("./include/post.inc.php");
+require_once("./include/user.inc.php");
+require_once("./include/threads.inc.php");
+require_once("./include/thread.inc.php");
+require_once("./include/messages.inc.php");
 
 html_draw_top();
 
 if (isset($HTTP_POST_VARS['submit'])) {
 
-  echo "Yes, thats right, this doesn't do anything at the moment, but it certainly had you going didn't it?<br /><br />";
-  echo "Method: ". $HTTP_POST_VARS['method']. "<br />\n";
-  echo "Search String: ". $HTTP_POST_VARS['search_string']. "<br />\n";
-  echo "Folder ID: ". $HTTP_POST_VARS['t_fid']. "<br />\n";
-
-  if (isset($HTTP_POST_VARS['to_other'])) {
-    echo "Messages To User: ". $HTTP_POST_VARS['to_other']. "<br />\n";
+  $db = db_connect();
+  
+  $sql = "SELECT THREAD.FID, THREAD.TID, POST.TID, POST.PID, POST.FROM_UID, POST.TO_UID, ";
+  $sql.= "UNIX_TIMESTAMP(POST.CREATED) AS CREATED, POST_CONTENT.CONTENT ";
+  $sql.= "FROM ". forum_table("THREAD"). " THREAD ";
+  $sql.= "LEFT JOIN ". forum_table("POST"). " ON (THREAD.TID = POST.TID) ";
+  $sql.= "LEFT JOIN ". forum_table("POST_CONTENT"). " ON (POST.PID = POST_CONTENT.PID AND POST.TID = POST_CONTENT.TID) ";
+  $sql.= "WHERE ";
+  
+  // Folder ID
+  
+  if ($HTTP_POST_VARS['fid'] > 0) {
+    $sql.= "THREAD.FID = ". $HTTP_POST_VARS['fid']. " ";
   }else{
-    echo "Messages To User: ". $HTTP_POST_VARS['t_to_uid']. "<br />\n";
+    $folders = threads_get_available_folders();
+    $sql.= "THREAD.FID in ($folders) ";
   }
   
-  echo "Date From: ". $HTTP_POST_VARS['posted_from']. "<br />\n";
-  echo "Date To: ". $HTTP_POST_VARS['posted_to']. "<br />\n";
-  echo "Order By: ". $HTTP_POST_VARS['order_by']. "<br />\n";
+  // User selection
   
-  if (isset($HTTP_POST_VARS['me_only'])) echo "Me only<br />\n";
+  if ($HTTP_POST_VARS['me_only'] != 'Y') {
+  
+    // To User
+  
+    if (empty($HTTP_POST_VARS['to_other']) && $HTTP_POST_VARS['to_uid'] > 0) {
+      $sql.= "AND POST.TO_UID = ". $HTTP_POST_VARS['to_uid']. " ";
+    }elseif (!empty($HTTP_POST_VARS['to_other'])) {
+      $touid = user_get_uid($HTTP_POST_VARS['to_other']);
+      if ($touid > -1) $sql.= "AND POST.TO_UID = ". $touid. " ";    
+    }
+  
+    // From User
+  
+    if (empty($HTTP_POST_VARS['from_other']) && $HTTP_POST_VARS['from_uid'] > 0) {
+      $sql.= "AND POST.FROM_UID = ". $HTTP_POST_VARS['from_uid']. " ";
+    }elseif (!empty($HTTP_POST_VARS['from_other'])) {
+      $fromuid = user_get_uid($HTTP_POST_VARS['from_other']);
+      if ($fromuid > -1) $sql.= "AND POST.FROM_UID = ". $fromuid. " ";      
+    }
+    
+  }else {
+  
+    $sql.= "AND POST.TO_UID = ". $HTTP_COOKIE_VARS['bh_sess_uid']. " OR POST.FROM_UID = ". $HTTP_COOKIE_VARS['bh_sess_uid']. " ";
+
+  }
+  
+  // Date Range
+  
+  $sql.= search_date_range($HTTP_POST_VARS['date_from'], $HTTP_POST_VARS['date_to']). " ";
+  
+  // Keywords
+  
+  if (!empty($HTTP_POST_VARS['search_string'])) {
+  
+    if ($HTTP_POST_VARS['method'] == 1) {
+  
+      $keywords = explode(' ', $HTTP_POST_VARS['search_string']);
+      foreach($keywords as $word) $sql.= "AND POST_CONTENT.CONTENT LIKE '%$word%' ";
+    
+    }elseif ($HTTP_POST_VARS['method'] == 2) {
+  
+      $sql.= "AND ";
+      $keywords = explode(' ', $HTTP_POST_VARS['search_string']);
+      foreach($keywords as $word) $sql.= "POST_CONTENT.CONTENT LIKE '%$word%' OR ";
+      $sql = substr($sql, 0, -3);
+    
+    }elseif ($HTTP_POST_VARS['method'] == 3) {
+  
+      $sql.= "AND POST_CONTENT.CONTENT LIKE '%". $HTTP_POST_VARS['search_string']. "%' ";
+
+    }
+    
+  }
+  
+  // Order by
+  
+  if ($HTTP_POST_VARS['order_by'] == 2) {
+    $sql.= "ORDER BY POST.CREATED DESC";
+  }elseif($HTTP_POST_VARS['order_by'] == 3) {
+    $sql.= "ORDER BY POST.CREATED";
+  }
+  
+  $result = db_query($sql, $db);
+  echo "Found: ". db_num_rows($result). " matches<br /><br />\n";
+  
+  while($row = db_fetch_array($result)) {
+  
+    $count++;
+    $message = messages_get($row['TID'], $row['PID']);
+    $threaddata = thread_get($row['TID']);
+    $closed = isset($threaddata['CLOSED']);
+    $foldertitle = folder_get_title($threaddata['FID']);
+    if($closed) $foldertitle .= " (closed)";
+    
+    echo "<div align=\"center\"><table width=\"96%\" border=\"0\"><tr><td>\n";
+    messages_top($foldertitle,stripslashes($threaddata['TITLE']),$threaddata['INTEREST']);
+    echo "</td></tr></table></div>\n";
+    
+    message_display($row['TID'], $message[0], $threaddata['LENGTH'], $row['PID'], true, false);
+    echo "<br />\n";
+    
+  }
 
   html_draw_bottom();
   exit;
@@ -69,7 +158,7 @@ if (isset($HTTP_POST_VARS['submit'])) {
 
 ?>
 <h1>Search Messages</h1>
-<form method="post" action="search.php" target="left">
+<form method="post" action="search.php">
 <table border="0" width="500" align="center">
   <tr>
     <td class="postbody" colspan="2">Search Discussions...</td>
@@ -86,15 +175,15 @@ if (isset($HTTP_POST_VARS['submit'])) {
   </tr>
   <tr>
     <td align="right" class="postbody">Folder(s):</td>
-    <td><?php echo folder_draw_dropdown(0); ?></td>
+    <td><?php echo folder_search_dropdown(); ?></td>
   </tr>
   <tr>
     <td align="right" class="postbody">To:</td>
-    <td class="postbody"><?php echo post_draw_to_dropdown(0); ?> or <?php echo form_input_text("to_other", "", 20); ?></td>
+    <td class="postbody"><?php echo search_draw_user_dropdown("to_uid"); ?> or <?php echo form_input_text("to_other", "", 20); ?></td>
   </tr>
   <tr>
     <td align="right" class="postbody">From:</td>
-    <td class="postbody"><?php echo post_draw_to_dropdown(0); ?> or <?php echo form_input_text("from_other", "", 20); ?></td>
+    <td class="postbody"><?php echo search_draw_user_dropdown("from_uid"); ?> or <?php echo form_input_text("from_other", "", 20); ?></td>
   </tr>
   <tr>
     <td align="right" class="postbody">Posted From:</td>
@@ -102,7 +191,7 @@ if (isset($HTTP_POST_VARS['submit'])) {
   </tr>
   <tr>
     <td align="right" class="postbody">Posted To:</td>
-    <td><?php echo form_dropdown_array("date_to", range(1, 13), array("Now", "Today", "Yesterday", "Day before yesterday", "1 week", "2 weeks", "3 weeks", "1 month", "2 months", "3 months", "6 months", "1 year", "Beginning of time"), 2); ?></td>
+    <td><?php echo form_dropdown_array("date_to", range(1, 12), array("Now", "Today", "Yesterday", "Day before yesterday", "1 week", "2 weeks", "3 weeks", "1 month", "2 months", "3 months", "6 months", "1 year"), 2); ?></td>
   </tr>
   <tr>
     <td align="right" class="postbody">Order by:</td>

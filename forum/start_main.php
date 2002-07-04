3534cfa909b914167e88814f90010c5a4eda8187
Matt Beale <matt@beehiveforum.co.uk>
2002-07-04 00:46:22 +0000

    Guest logon fixes

--- forum/start_main.php
+++ forum/start_main.php
@@ -1,9 +1,78 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-<body>
-<P><IMG src="http://people.delphiforums.com/simon_r_/npcff.jpg"></P></CENTER>
-<P><STRONG>H</STRONG>ello and welcome to the NPCFF forum! Originally a replacement for the old PCF Delphi forum, we've grown into a thriving community:</P>
-<P><STRONG>Y</STRONG>ou can find out&nbsp;about the forum and our guidelines&nbsp;<A href="http://people.delphiforums.com/simon_r_/faq.html" target=_blank><B>HERE</B></A>&nbsp;at the NPCFF Quick FAQ or browse through the interactive version &nbsp;<A href="http://www.hodcroft.net/faq" target=_blank><B>HERE</B></A>&nbsp; or leap right in and start reading.</P>
-<P><STRONG>G</STRONG>uidelines:<BR>We don't mind a little mild swearing, but try not to over do it, and we're not very tolerant about spam, homophobia, rascism and general bigotry... same as most other places!</P>
-<P><STRONG>T</STRONG>he Moderators are generally on hand to keep things running smoothly, but you can help out by not posting off topic if you can help it!</P>
-<P><STRONG>S</STRONG>o on behalf of the other posters, come on in!</P>
+<head>
+<title>Project Beehive</title>
+<style type="text/css">
+<!--
+.bodytext {
+	font-family: Verdana, Arial, Helvetica, sans-serif; 
+	font-size: 11px; 
+	font-style: normal; 
+	line-height: 13px; 
+	font-weight: normal; 
+	color: #666666;
+        background-color: #EAEFF4
+	}
+.title {
+	font-family: Verdana, Arial, Helvetica, sans-serif; 
+	font-size: 18px; 
+	font-style: normal; 
+	font-weight: bold; 
+	color: #ffffff;
+        background-color: #A6BED7
+	}
+a { 
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-size: 11px;
+	line-height: 13px;
+	font-weight: normal;
+	color: #333399; 
+	text-decoration: underline
+	}
+-->
+</style>
+</head>
+
+<body bgcolor="#EAEFF4" text="#666666" link="#333399" vlink="#993333" alink="#333399" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<table width="100%" border="0" cellspacing="0" cellpadding="8">
+  <tr> 
+    <td class="title">Project: Beehive Forum</td>
+  </tr>
+  <tr> 
+    <td class="bodytext"><a href="http://sourceforge.net/projects/beehiveforum/" target="_blank">Home</a> | FAQ | <a href="http://sourceforge.net/docman/?group_id=50772" target="_blank">Docs</a> | <a href="http://sourceforge.net/project/showfiles.php?group_id=50772" target="_blank">Download</a> | <a href="../forums.php">Live Forums</a></td>
+  </tr>
+  <tr>
+    <td height="1" class="title"></td>
+  </tr>
+  <tr> 
+    <td valign="top" class="bodytext">
+      Hello and welcome to the the Project Beehive test forum.<br /><br />
+      Guidelines:<br />
+      <ol>
+        <li>First and foremost this is a test forum and it should be kept that way. By all means post messages, test it out, but please don't invite all your mates over here and adpot it as your permenant residence.<br /><br /></li>
+        <li>We don't mind a little mild swearing, but try not to over do it, and we're not very tolerant about spam, homophobia, rascism and general bigotry... same as most other places!<br /><br /></li>
+      </ol>
+      As unlikely as it sounds, this forum is highlighy likely to
+      contain bugs. I know, I know how could it possibly contain bugs
+      with such mind numbingly brilliant developers aboard? Enough
+      of the <del>sarcasm</del> Ego inflating I think. In all seriousness
+      if you find a bug please leave a post detailing what it is that
+      happened, what you expected to happen and what it is you were doing
+      when it happended. All of this information will help considerably
+      in locating the problem and getting it sorted. Even better if you know
+      how and why it happended, right down to the line number in the source
+      code, please tell us, as it will make things even easier.<br /><br />
+      Enough gibberish for now, come on in!
+    </td>
+  </tr>
+  <tr>
+    <td height="1" class="title"></td>
+  </tr>
+  <tr> 
+    <td align="right">
+      <a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=50772&type=5" width="210" height="62" border="0" /></a>
+    </td>
+  </tr>
+</table>
 </body>
+</html>

147a4b7ed0d0b75c068907c5389b6623e8b0cae1
Mark Rendle <no-reply@beehiveforum.co.uk>
2002-07-05 16:17:04 +0000

    Added links to beehive.inc.php

--- forum/include/beehive.inc.php
+++ forum/include/beehive.inc.php
@@ -25,7 +25,15 @@ function draw_beehive_bar()
 {
 ?>
 <div align="center"><table width="96%" class="posthead"><tr>
-<td width="60%" class="smalltext">Beehive Forum 0.1.2 | FAQ | Docs | Support</td>
+<td width="60%" class="smalltext">
+Beehive Forum 0.1.2
+&nbsp;|&nbsp;
+<a href="http://beehiveforum.sf.net/faq" target="_blank">FAQ</a>
+&nbsp;|&nbsp;
+<a href="http://sourceforge.net/docman/?group_id=50772" target="_blank">Docs</a>
+&nbsp;|&nbsp;
+<a href="http://sourceforge.net/tracker/?group_id=50772&atid=460926" target="_blank">Support</a>
+</td>
 <td width="40%" align="right" class="smalltext">&copy;2002
 <a href="http://beehiveforum.sourceforge.net/" target="_blank">Project BeehiveForum</a>
 </td></tr></table></div>

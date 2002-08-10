56d12f79e8ab0296359ccf7795d426deddd56741
Matt Beale <matt@beehiveforum.co.uk>
2002-08-10 16:09:24 +0000

    Guest account fixed

--- docs/schema.sql
+++ docs/schema.sql
@@ -1,9 +1,13 @@
-# Beehive Forum Database Creation
-# version 0.1
-# http://beehiveforum.sourceforge.net/
+# phpMyAdmin MySQL-Dump
+# version 2.2.5
+# http://phpwizard.net/phpMyAdmin/
+# http://phpmyadmin.sourceforge.net/ (download page)
 #
-# Schema generated using phpMyAdmin
-# (http://phpmyadmin.sourceforge.net)
+# Host: mysql.sourceforge.net
+# Generation Time: Aug 10, 2002 at 09:08 AM
+# Server version: 3.23.36
+# PHP Version: 4.1.2
+# Database : `beehiveforum`
 # --------------------------------------------------------
 
 #
@@ -15,7 +19,7 @@ CREATE TABLE DEDUPE (
   DDKEY char(32) default NULL,
   PRIMARY KEY  (UID)
 ) TYPE=MyISAM;
-
+# --------------------------------------------------------
 
 #
 # Table structure for table `FOLDER`
@@ -27,19 +31,13 @@ CREATE TABLE FOLDER (
   ACCESS_LEVEL tinyint(4) default '0',
   PRIMARY KEY  (FID)
 ) TYPE=MyISAM;
-
-#
-# Dumping data for table `FOLDER`
-#
-
-INSERT INTO FOLDER VALUES (1, 'General', 0);
 # --------------------------------------------------------
 
 #
-# Table structure for table `poll`
+# Table structure for table `POLL`
 #
 
-CREATE TABLE poll (
+CREATE TABLE POLL (
   TID mediumint(8) unsigned NOT NULL default '0',
   O1 varchar(255) default NULL,
   O1_VOTES mediumint(8) unsigned default '0',
@@ -57,11 +55,6 @@ CREATE TABLE poll (
   SHOWRESULTS tinyint(1) NOT NULL default '1',
   KEY TID (TID)
 ) TYPE=MyISAM;
-
-#
-# Dumping data for table `POLL`
-#
-
 # --------------------------------------------------------
 
 #
@@ -74,11 +67,6 @@ CREATE TABLE POLL_VOTES (
   VOTE tinyint(3) unsigned NOT NULL default '0',
   TSTAMP timestamp(14) NOT NULL
 ) TYPE=MyISAM;
-
-#
-# Dumping data for table `POLL_VOTES`
-#
-
 # --------------------------------------------------------
 
 #
@@ -88,21 +76,15 @@ CREATE TABLE POLL_VOTES (
 CREATE TABLE POST (
   TID mediumint(8) unsigned NOT NULL default '0',
   PID mediumint(8) unsigned NOT NULL auto_increment,
-  REPLY_TO_PID mediumint(8) unsigned NOT NULL default '0',
-  FROM_UID mediumint(8) unsigned NOT NULL default '0',
-  TO_UID mediumint(8) unsigned NOT NULL default '0',
+  REPLY_TO_PID mediumint(8) unsigned default NULL,
+  FROM_UID mediumint(8) unsigned default NULL,
+  TO_UID mediumint(8) unsigned default NULL,
   VIEWED datetime default NULL,
   CREATED timestamp(14) NOT NULL,
   STATUS tinyint(4) default '0',
   PRIMARY KEY  (TID,PID),
   KEY TO_UID (TO_UID)
 ) TYPE=MyISAM;
-
-#
-# Dumping data for table `POST`
-#
-
-INSERT INTO POST VALUES (1, 1, 0, 1, 0, NULL, NOW(), 0);
 # --------------------------------------------------------
 
 #
@@ -120,7 +102,7 @@ CREATE TABLE POST_ATTACHMENT_FILES (
   KEY AID (AID),
   KEY HASH (HASH)
 ) TYPE=MyISAM;
-
+# --------------------------------------------------------
 
 #
 # Table structure for table `POST_ATTACHMENT_IDS`
@@ -133,7 +115,7 @@ CREATE TABLE POST_ATTACHMENT_IDS (
   KEY AID (AID),
   KEY ix_pattid_1 (TID,PID)
 ) TYPE=MyISAM;
-
+# --------------------------------------------------------
 
 #
 # Table structure for table `POST_CONTENT`
@@ -146,12 +128,6 @@ CREATE TABLE POST_CONTENT (
   PRIMARY KEY  (TID,PID),
   FULLTEXT KEY CONTENT (CONTENT)
 ) TYPE=MyISAM;
-
-#
-# Dumping data for table `POST_CONTENT`
-#
-
-INSERT INTO POST_CONTENT VALUES (1, 1, 'Welcome to your new Beehive Forum');
 # --------------------------------------------------------
 
 #
@@ -164,17 +140,6 @@ CREATE TABLE PROFILE_ITEM (
   NAME varchar(64) default NULL,
   PRIMARY KEY  (PIID)
 ) TYPE=MyISAM;
-
-#
-# Dumping data for table `PROFILE_ITEM`
-#
-
-INSERT INTO PROFILE_ITEM VALUES (1, 1, 'Location');
-INSERT INTO PROFILE_ITEM VALUES (2, 1, 'Age');
-INSERT INTO PROFILE_ITEM VALUES (3, 1, 'Gender');
-INSERT INTO PROFILE_ITEM VALUES (4, 1, 'Quote');
-INSERT INTO PROFILE_ITEM VALUES (5, 1, 'Occupation');
-INSERT INTO PROFILE_ITEM VALUES (6, 1, 'Birthday (DD/MM)');
 # --------------------------------------------------------
 
 #
@@ -186,12 +151,6 @@ CREATE TABLE PROFILE_SECTION (
   NAME varchar(64) default NULL,
   PRIMARY KEY  (PSID)
 ) TYPE=MyISAM;
-
-#
-# Dumping data for table `PROFILE_SECTION`
-#
-
-INSERT INTO PROFILE_SECTION VALUES (1, 'Personal');
 # --------------------------------------------------------
 
 #
@@ -200,8 +159,8 @@ INSERT INTO PROFILE_SECTION VALUES (1, 'Personal');
 
 CREATE TABLE THREAD (
   TID mediumint(8) unsigned NOT NULL auto_increment,
-  FID mediumint(8) unsigned NOT NULL default '0',
-  BY_UID mediumint(8) unsigned NOT NULL default '0',
+  FID mediumint(8) unsigned default NULL,
+  BY_UID mediumint(8) unsigned default NULL,
   TITLE varchar(64) default NULL,
   LENGTH mediumint(8) unsigned default NULL,
   POLL_FLAG char(1) default NULL,
@@ -211,20 +170,14 @@ CREATE TABLE THREAD (
   KEY ix_thread_fid (FID),
   KEY BY_UID (BY_UID)
 ) TYPE=MyISAM;
-
-#
-# Dumping data for table `THREAD`
-#
-
-INSERT INTO THREAD VALUES (1, 1, 1, 'Welcome', 1, 'N', NOW(), NULL);
 # --------------------------------------------------------
 
 #
 # Table structure for table `USER`
 #
 
-CREATE TABLE USER (  
-  UID mediumint(8) NOT NULL auto_increment,  
+CREATE TABLE USER (
+  UID mediumint(8) unsigned NOT NULL auto_increment,
   LOGON varchar(32) default NULL,
   PASSWD varchar(32) default NULL,
   NICKNAME varchar(32) default NULL,
@@ -233,14 +186,6 @@ CREATE TABLE USER (
   LAST_LOGON timestamp(14) NOT NULL,
   PRIMARY KEY  (UID)
 ) TYPE=MyISAM;
-
-#
-# Dumping data for table `USER`
-#
-
-INSERT INTO USER VALUES (0, 'GUEST', '084e0343a0486ff05530df6c705c8bb4', 'Guest', 'guest@email.com', 1, NOW());
-UPDATE USER SET UID = 0 WHERE LOGON = 'GUEST' AND PASSWD = '084e0343a0486ff05530df6c705c8bb4';
-INSERT INTO USER VALUES (1, 'ADMIN', 'b60eb83bf533eecf1bde65940925a981', 'Administrator', 'your@email.com', 56, NOW());
 # --------------------------------------------------------
 
 #
@@ -248,13 +193,12 @@ INSERT INTO USER VALUES (1, 'ADMIN', 'b60eb83bf533eecf1bde65940925a981', 'Admini
 #
 
 CREATE TABLE USER_FOLDER (
-  UID mediumint(8) unsigned NOT NULL default '0',
-  FID mediumint(8) unsigned NOT NULL default '0',
+  UID mediumint(8) unsigned default NULL,
+  FID mediumint(8) unsigned default NULL,
   INTEREST tinyint(4) default '0',
   ALLOWED tinyint(4) default '0',
   KEY UID (UID)
 ) TYPE=MyISAM;
-
 # --------------------------------------------------------
 
 #
@@ -262,12 +206,11 @@ CREATE TABLE USER_FOLDER (
 #
 
 CREATE TABLE USER_PEER (
-  UID mediumint(8) unsigned NOT NULL default '0',
-  PEER_UID mediumint(8) unsigned NOT NULL default '0',
+  UID mediumint(8) unsigned default NULL,
+  PEER_UID mediumint(8) unsigned default NULL,
   RELATIONSHIP tinyint(4) default NULL,
   KEY UID (UID)
 ) TYPE=MyISAM;
-
 # --------------------------------------------------------
 
 #
@@ -275,7 +218,7 @@ CREATE TABLE USER_PEER (
 #
 
 CREATE TABLE USER_PREFS (
-  UID mediumint(8) unsigned NOT NULL default '0',
+  UID mediumint(8) unsigned default NULL,
   FIRSTNAME varchar(32) default NULL,
   LASTNAME varchar(32) default NULL,
   HOMEPAGE_URL varchar(255) default NULL,
@@ -286,13 +229,10 @@ CREATE TABLE USER_PREFS (
   MARK_AS_OF_INT char(1) default NULL,
   POSTS_PER_PAGE tinyint(3) unsigned default NULL,
   FONT_SIZE tinyint(3) unsigned default NULL,
+  STYLE varchar(255) default NULL,
+  KEY STYLE (STYLE),
   KEY UID (UID)
 ) TYPE=MyISAM;
-
-#
-# Dumping data for table `USER_PREFS`
-#
-
 # --------------------------------------------------------
 
 #
@@ -300,11 +240,10 @@ CREATE TABLE USER_PREFS (
 #
 
 CREATE TABLE USER_PROFILE (
-  UID mediumint(8) unsigned NOT NULL default '0',
-  PIID mediumint(8) unsigned NOT NULL default '0',
+  UID mediumint(8) unsigned default NULL,
+  PIID mediumint(8) unsigned default NULL,
   ENTRY varchar(255) default NULL
 ) TYPE=MyISAM;
-
 # --------------------------------------------------------
 
 #
@@ -312,12 +251,11 @@ CREATE TABLE USER_PROFILE (
 #
 
 CREATE TABLE USER_SIG (
-  UID mediumint(8) unsigned NOT NULL default '0',
+  UID mediumint(8) unsigned default NULL,
   CONTENT text,
   HTML char(1) default NULL,
   KEY ix_user_sig (UID)
 ) TYPE=MyISAM;
-
 # --------------------------------------------------------
 
 #
@@ -325,12 +263,11 @@ CREATE TABLE USER_SIG (
 #
 
 CREATE TABLE USER_THREAD (
-  UID mediumint(8) unsigned NOT NULL default '0',
-  TID mediumint(8) unsigned NOT NULL default '0',
+  UID mediumint(8) unsigned default NULL,
+  TID mediumint(8) unsigned default NULL,
   LAST_READ mediumint(8) unsigned default NULL,
   LAST_READ_AT datetime default NULL,
   INTEREST tinyint(4) default NULL,
   UNIQUE KEY ix_user_thread_1 (UID,TID)
 ) TYPE=MyISAM;
-
 

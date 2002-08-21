6f6606a2b25b4bb1ea4650a48a3cce06eb9d2c70
HeyRatFans <matt@heyratfans.co.uk>
2002-08-21 11:14:30 +0000

    Ensure all files are UNIX ANSI formatted and all whitespace is removed from *.inc.php

--- forum/delete.php
+++ forum/delete.php
@@ -1,1 +1,175 @@
-<?php/*======================================================================Copyright Project BeehiveForum 2002This file is part of BeehiveForum.BeehiveForum is free software; you can redistribute it and/or modifyit under the terms of the GNU General Public License as published bythe Free Software Foundation; either version 2 of the License, or(at your option) any later version.BeehiveForum is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See theGNU General Public License for more details.You should have received a copy of the GNU General Public Licensealong with Beehive; if not, write to the Free SoftwareFoundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307USA======================================================================*///Check logged in statusrequire_once("./include/session.inc.php");require_once("./include/header.inc.php");if(!bh_session_check()){    $uri = "./logon.php?final_uri=". urlencode(get_request_uri());    header_redirect($uri);    }if(isset($HTTP_POST_VARS['cancel'])) {    $uri = "./discussion.php?msg=" . $HTTP_POST_VARS['t_back'];    header_redirect($uri);    }require_once("./include/html.inc.php");if($HTTP_COOKIE_VARS['bh_sess_uid'] == 0) {	html_guest_error();	exit;}require_once("./include/user.inc.php");require_once("./include/post.inc.php");require_once("./include/format.inc.php");require_once("./include/folder.inc.php");require_once("./include/thread.inc.php");require_once("./include/messages.inc.php");require_once("./include/fixhtml.inc.php");require_once("./include/edit.inc.php");require_once("./include/poll.inc.php");$valid = true;if(isset($HTTP_POST_VARS['submit'])) {    $delete_msg = $HTTP_POST_VARS['t_msg'];    list($tid, $pid) = explode(".", $delete_msg);    }else {    if(isset($HTTP_GET_VARS['msg'])) {            $delete_msg = $HTTP_GET_VARS['msg'];        list($tid, $pid) = explode(".",$delete_msg);        $back = $HTTP_GET_VARS['back'];            }else {            $valid = false;        $error_html = "<h2>No message specified for deleting</h2>";            }        if(isset($tid) && isset($pid)) {            $preview_message = messages_get($tid, $pid, 1);                if(count($preview_message) > 0) {                    $preview_message['CONTENT'] = message_get_content($tid, $pid);                        if($HTTP_COOKIE_VARS['bh_sess_uid'] != $preview_message['FROM_UID'] && !perm_is_moderator()) {                edit_refuse();                exit;            }                        $to_uid = $preview_message['TO_UID'];            $from_uid = $preview_message['FROM_UID'];                    }else {                    $valid = false;            $error_html = "<h2>Message " . $HTTP_GET_VARS['msg'] . " was not found</h2>";                    }    }}html_draw_top_script();if ($valid) {    if(isset($HTTP_POST_VARS['submit'])) {                if (post_delete($tid, $pid)) {                    echo "<p>&nbsp;</p>";            echo "<p>&nbsp;</p>";            echo "<div align=\"center\">";            echo "<p>Post deleted successfully</p>";            echo "<p><a href=\"discussion.php?msg=" . $HTTP_POST_VARS['t_back'];            echo "\" target=\"_self\">Return to messages</a></p>";            echo "</div>";                        html_draw_bottom();            exit;                    }else {                    $error_html = "<h2>Error deleting post</h2>";                    }    }    echo "<h2>Delete this message:</h2>";    echo "<h2>" . thread_get_title($tid) . "</h2>";        if($to_uid == 0) {            $preview_message['TLOGON'] = "ALL";        $preview_message['TNICK'] = "ALL";            }else {            $preview_tuser = user_get($to_uid);        $preview_message['TLOGON'] = $preview_tuser['LOGON'];        $preview_message['TNICK'] = $preview_tuser['NICKNAME'];            }        $preview_tuser = user_get($from_uid);    $preview_message['FLOGON'] = $preview_tuser['LOGON'];    $preview_message['FNICK'] = $preview_tuser['NICKNAME'];        if (thread_is_poll($tid) && $pid == 1) {          poll_display($tid, $preview_message, 0, 0, false);          }else {          message_display(0, $preview_message, 0, 0, false);          }}if(isset($error_html)) echo $error_html;echo "<p><form name=\"f_delete\" action=\"" . $HTTP_SERVER_VARS['PHP_SELF'] . "\" method=\"POST\" target=\"_self\">";echo form_input_hidden("t_msg",$delete_msg);echo form_input_hidden("t_back",$back);echo form_submit("submit","Delete");echo "&nbsp;".form_submit("cancel","Cancel");echo "</form>\n";echo "<p>&nbsp;&nbsp;</p>\n";html_draw_bottom();?>
+<?php
+
+/*======================================================================
+Copyright Project BeehiveForum 2002
+
+This file is part of BeehiveForum.
+
+BeehiveForum is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BeehiveForum is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Beehive; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+USA
+======================================================================*/
+
+//Check logged in status
+require_once("./include/session.inc.php");
+require_once("./include/header.inc.php");
+
+if(!bh_session_check()){
+
+    $uri = "./logon.php?final_uri=". urlencode(get_request_uri());
+    header_redirect($uri);
+    
+}
+
+if(isset($HTTP_POST_VARS['cancel'])) {
+
+    $uri = "./discussion.php?msg=" . $HTTP_POST_VARS['t_back'];
+    header_redirect($uri);
+    
+}
+
+require_once("./include/html.inc.php");
+
+if($HTTP_COOKIE_VARS['bh_sess_uid'] == 0) {
+	html_guest_error();
+	exit;
+}
+
+require_once("./include/user.inc.php");
+require_once("./include/post.inc.php");
+require_once("./include/format.inc.php");
+require_once("./include/folder.inc.php");
+require_once("./include/thread.inc.php");
+require_once("./include/messages.inc.php");
+require_once("./include/fixhtml.inc.php");
+require_once("./include/edit.inc.php");
+require_once("./include/poll.inc.php");
+
+$valid = true;
+
+if(isset($HTTP_POST_VARS['submit'])) {
+
+    $delete_msg = $HTTP_POST_VARS['t_msg'];
+    list($tid, $pid) = explode(".", $delete_msg);
+    
+}else {
+
+    if(isset($HTTP_GET_VARS['msg'])) {
+    
+        $delete_msg = $HTTP_GET_VARS['msg'];
+        list($tid, $pid) = explode(".",$delete_msg);
+        $back = $HTTP_GET_VARS['back'];
+        
+    }else {
+    
+        $valid = false;
+        $error_html = "<h2>No message specified for deleting</h2>";
+        
+    }
+    
+    if(isset($tid) && isset($pid)) {
+    
+        $preview_message = messages_get($tid, $pid, 1);
+        
+        if(count($preview_message) > 0) {
+        
+            $preview_message['CONTENT'] = message_get_content($tid, $pid);
+            
+            if($HTTP_COOKIE_VARS['bh_sess_uid'] != $preview_message['FROM_UID'] && !perm_is_moderator()) {
+                edit_refuse();
+                exit;
+            }
+            
+            $to_uid = $preview_message['TO_UID'];
+            $from_uid = $preview_message['FROM_UID'];
+            
+        }else {
+        
+            $valid = false;
+            $error_html = "<h2>Message " . $HTTP_GET_VARS['msg'] . " was not found</h2>";
+            
+        }
+    }
+}
+
+html_draw_top_script();
+
+if ($valid) {
+
+    if(isset($HTTP_POST_VARS['submit'])) {
+        
+        if (post_delete($tid, $pid)) {
+        
+            echo "<p>&nbsp;</p>";
+            echo "<p>&nbsp;</p>";
+            echo "<div align=\"center\">";
+            echo "<p>Post deleted successfully</p>";
+            echo "<p><a href=\"discussion.php?msg=" . $HTTP_POST_VARS['t_back'];
+            echo "\" target=\"_self\">Return to messages</a></p>";
+            echo "</div>";
+            
+            html_draw_bottom();
+            exit;
+            
+        }else {
+        
+            $error_html = "<h2>Error deleting post</h2>";
+            
+        }
+    }
+
+    echo "<h2>Delete this message:</h2>";
+    echo "<h2>" . thread_get_title($tid) . "</h2>";
+    
+    if($to_uid == 0) {
+    
+        $preview_message['TLOGON'] = "ALL";
+        $preview_message['TNICK'] = "ALL";
+        
+    }else {
+    
+        $preview_tuser = user_get($to_uid);
+        $preview_message['TLOGON'] = $preview_tuser['LOGON'];
+        $preview_message['TNICK'] = $preview_tuser['NICKNAME'];
+        
+    }
+    
+    $preview_tuser = user_get($from_uid);
+    $preview_message['FLOGON'] = $preview_tuser['LOGON'];
+    $preview_message['FNICK'] = $preview_tuser['NICKNAME'];
+    
+    if (thread_is_poll($tid) && $pid == 1) {
+    
+      poll_display($tid, $preview_message, 0, 0, false);
+      
+    }else {
+    
+      message_display(0, $preview_message, 0, 0, false);
+      
+    }
+}
+
+if(isset($error_html)) echo $error_html;
+
+echo "<p><form name=\"f_delete\" action=\"" . $HTTP_SERVER_VARS['PHP_SELF'] . "\" method=\"POST\" target=\"_self\">";
+echo form_input_hidden("t_msg",$delete_msg);
+echo form_input_hidden("t_back",$back);
+echo form_submit("submit","Delete");
+echo "&nbsp;".form_submit("cancel","Cancel");
+echo "</form>\n";
+echo "<p>&nbsp;&nbsp;</p>\n";
+
+html_draw_bottom();
+
+?>
--- forum/profile.php
+++ forum/profile.php
@@ -1,27 +1,34 @@
 <?php
 
+
 /*======================================================================
 Copyright Project BeehiveForum 2002
 
+
 This file is part of BeehiveForum.
+
 
 BeehiveForum is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
+
 BeehiveForum is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
+
 You should have received a copy of the GNU General Public License
 along with Beehive; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
+
 // Frameset for thread list and messages
+
 
 //Check logged in status
 require_once("./include/session.inc.php");
@@ -34,25 +41,121 @@ if(!bh_session_check()){
     
 }
 
-// Disable caching when showing logon page
-require_once("./include/header.inc.php");
-if(!isset($HTTP_COOKIE_VARS['bh_sess_uid'])){
-    header_no_cache();
+require_once("./include/perm.inc.php");
+require_once("./include/html.inc.php");
+
+
+if($HTTP_COOKIE_VARS['bh_sess_uid'] == 0) {
+	html_guest_error();
+	exit;
 }
 
-require_once("./include/config.inc.php");
+
+require_once("./include/forum.inc.php");
+require_once("./include/form.inc.php");
+require_once("./include/db.inc.php");
+require_once("./include/user_profile.inc.php");
+require_once("./include/constants.inc.php");
+
+
+html_draw_top();
+
+
+echo "<h1>Edit Profile</h1>\n";
+echo "<br />\n";
+
+
+$uid = $HTTP_COOKIE_VARS['bh_sess_uid'];
+
+
+// Do updates
+if(isset($HTTP_POST_VARS['submit'])){
+    for($i=0;$i<$HTTP_POST_VARS['t_count'];$i++){
+        if($HTTP_POST_VARS['t_entry_'.$i] != $HTTP_POST_VARS['t_old_entry_'.$i]){
+            $entry = trim($HTTP_POST_VARS['t_entry_'.$i]);
+            if($HTTP_POST_VARS['t_new_'.$i] == "Y"){
+                user_profile_create($uid,$HTTP_POST_VARS['t_piid_'.$i],$entry);
+            } else {
+                user_profile_update($uid,$HTTP_POST_VARS['t_piid_'.$i],$entry);
+            }
+        }
+    }
+    echo "<p>Profile updated.</p>";
+}
+
+
+// Draw the form
+echo "<table width=\"600\" class=\"box\">\n  <tr>\n    <td class=\"posthead\">\n";
+
+
+echo "      <form name=\"f_profile\" action=\"" . $HTTP_SERVER_VARS['PHP_SELF'] . "\" method=\"POST\">\n";
+echo "        <table class=\"posthead\" width=\"100%\">\n";
+
+
+$db = db_connect();
+
+
+$sql = "select PROFILE_SECTION.PSID, PROFILE_SECTION.NAME as SECTION_NAME, ";
+$sql.= "PROFILE_ITEM.PIID, PROFILE_ITEM.NAME as ITEM_NAME, ";
+$sql.= "USER_PROFILE.PIID as CHECK_PIID, USER_PROFILE.ENTRY ";
+$sql.= "from " . forum_table("PROFILE_SECTION") . " PROFILE_SECTION, ";
+$sql.= forum_table("PROFILE_ITEM") . " PROFILE_ITEM ";
+$sql.= "left join " . forum_table("USER_PROFILE") . " USER_PROFILE ";
+$sql.= "on (USER_PROFILE.PIID = PROFILE_ITEM.PIID and USER_PROFILE.UID = $uid) ";
+$sql.= "where PROFILE_ITEM.PSID = PROFILE_SECTION.PSID ";
+$sql.= "order by PROFILE_SECTION.PSID, PROFILE_ITEM.PIID";
+
+
+$result = db_query($sql,$db);
+$result_count = db_num_rows($result);
+
+
+if($result_count == 0){
+    echo "<p>The forum owner has not set up Profiles.</p>";
+    html_draw_bottom();
+    exit;
+}
+
+
+$last_psid = -1;
+
+
+for($i=0;$i<$result_count;$i++){
+
+
+    $row = db_fetch_array($result);
+
+
+    $new = isset($row['CHECK_PIID']) ? "N" : "Y";
+    $row['ENTRY'] = isset($row['ENTRY']) ? stripslashes($row['ENTRY']) : "";
+
+
+    if($row['PSID'] != $last_psid){
+        if($last_psid > -1){
+            echo "          <tr>\n            <td colspan=\"2\">&nbsp;</td>\n          </tr>\n";
+        }
+        echo "          <tr>\n            <td class=\"subhead\" colspan=\"2\">". $row['SECTION_NAME']. "</td>\n          </tr>\n";
+        $last_psid = $row['PSID'];
+    }
+
+
+    echo "          <tr>\n            <td>".$row['ITEM_NAME'].form_input_hidden("t_piid_$i",$row['PIID'])."</td>\n";
+    echo "            <td nowrap=\"nowrap\" align=\"right\">:&nbsp;". form_field("t_entry_$i",$row['ENTRY'], 60, 255);
+    echo form_input_hidden("t_old_entry_$i", $row['ENTRY']);
+    echo form_input_hidden("t_new_$i", $new)."&nbsp;&nbsp;</td>\n          </tr>\n";
+}
+
+
+echo "          <tr>\n            <td colspan=\"2\">&nbsp;</td>\n          </tr>\n";
+echo "          <tr>\n            <td colspan=\"2\" align=\"right\">";
+echo form_input_hidden("t_count", $result_count);
+echo form_submit("submit", "Submit");
+echo "</td>\n          </tr>\n        </table>\n";
+echo "      </form>\n";
+echo "    </td>\n  </tr>\n</table>\n";
+
+
+html_draw_bottom();
+
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "DTD/xhtml1-frameset.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-		<title><?= $forum_name ?></title>
-		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-		<link rel="stylesheet" href="./styles/style.css" type="text/css">
-	</head>
-	<frameset rows="20,*" border="0">
-          <frame src="./nav.php" name="nav" border="0" scrolling="no" marginwidth="0" marginheight="0" noresize>
-          <frame src="./edit_profile.php" name="prefs" border="0" scrolling="auto" marginwidth="0" marginheight="0" noresize>
-        </frameset>	
-
-</html>

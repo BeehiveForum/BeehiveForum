5d66f33481c0bda43aee75d8fb94ac455dd3fd99
HeyRatFans <matt@heyratfans.co.uk>
2002-11-01 21:55:39 +0000

    Ignore folder fixes and new scheme.sql

--- docs/schema.sql
+++ docs/schema.sql
@@ -276,6 +276,7 @@ CREATE TABLE USER_PREFS (
   POSTS_PER_PAGE tinyint(3) unsigned default NULL,
   FONT_SIZE tinyint(3) unsigned default NULL,
   STYLE varchar(255) default NULL,
+  VIEW_SIGS char(1) default NULL,
   KEY STYLE (STYLE),
   KEY UID (UID)
 ) TYPE=MyISAM;

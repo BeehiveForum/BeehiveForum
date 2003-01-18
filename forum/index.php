5b5f691a7a6baa5e7f116e79d9bcc6b7bbd97cef
HeyRatFans <matt@heyratfans.co.uk>
2003-01-18 12:23:37 +0000

    Proper and full DTD for index.php

--- forum/index.php
+++ forum/index.php
@@ -40,7 +40,7 @@ if (!file_exists($top_html)) {
 }
 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "DTD/xhtml1-frameset.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <title><?php echo $forum_name; ?></title>

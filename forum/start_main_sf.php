c0daf70582885630eb7c8d684518845a9401f295
Chris Hodcroft <no-reply@beehiveforum.co.uk>
2003-02-11 18:54:20 +0000

    Final preparation for 0.3 release.
    1. Wrote a new start page that isn't the same as the one on the
    SourceForge development forum (which is now in start_main_sf.php)
    
    2. Updated documentation (upgrading from 0.2 to 0.3 in readme.txt)
    
    3. Changed the version number from 0.3ish to 0.3. I am truly a PHP god
    ;)

--- forum/start_main_sf.php
+++ forum/start_main_sf.php
@@ -24,7 +24,7 @@ a            { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 11
 <body class="bodytext">
 <table width="100%" border="0" cellspacing="0" cellpadding="8">
   <tr>
-    <td class="title">Welcome to your new Beehive Forum!</td>
+    <td class="title">Project: Beehive Forum</td>
   </tr>
   <tr>
     <td class="bodytext"><a href="http://sourceforge.net/projects/beehiveforum/" target="_blank">Home</a> | <a href="http://beehiveforum.net/faq">FAQ</a> | <a href="http://sourceforge.net/docman/?group_id=50772" target="_blank">Docs</a> | <a href="http://sourceforge.net/project/showfiles.php?group_id=50772" target="_blank"> Download</a> | <a href="../forums.php">Live Forums</a></td>
@@ -34,11 +34,21 @@ a            { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 11
   </tr>
   <tr>
     <td valign="top" class="bodytext">
-      <p>You can modify this start page from the admin interface.</p>
+      <p>Hello and welcome to the Project Beehive test forum.</p>
+      <p>Guidelines:</p>
+      <ol>
+        <li><p>First and foremost this is a test forum and it should be kept that way. By all means post messages, test it out, but please don't invite all your mates over here and adopt it as your permanent residence.</p></li>
+        <li><p>We don't mind a little mild swearing, but try not to over do it, and we're not very tolerant about spam, homophobia, racism and general bigotry... same as most other places!</p></li>
+      </ol>
+      <p>As unlikely as it sounds, this forum is highly likely to contain bugs. I know, I know how could it possibly contain bugs with such mind numbingly brilliant developers aboard? Enough of the <del>sarcasm</del>  Ego inflating I think. In all seriousness if you find a bug please leave a post detailing what it is that happened, what you expected to happen and what it is you were doing when it happened. All of this information will help considerably in locating the problem and getting it sorted. Even better if you know how and why it appended, right down to the line number in the source code, please tell us, as it will make things even easier.</p>
+      <p>Enough gibberish for now, come on in!</p>
     </td>
   </tr>
   <tr>
     <td height="1" class="title">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="right"><a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=50772&type=5" width="210" height="62" border="0" /></a></td>
   </tr>
 </table>
 </body>

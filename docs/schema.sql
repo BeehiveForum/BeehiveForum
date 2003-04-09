bf0418c349a3a9887b93f23cddce7ec05414dafc
HeyRatFans <matt@heyratfans.co.uk>
2003-04-09 22:12:03 +0000

    Updated the schema.sql so it is compliant with the latest 0.4-dev changes.

--- docs/schema.sql
+++ docs/schema.sql
@@ -8,6 +8,26 @@
 # --------------------------------------------------------
 
 #
+# Table structure for table `ADMIN_LOG`
+#
+
+CREATE TABLE ADMIN_LOG (
+  LOG_ID mediumint(8) unsigned NOT NULL auto_increment,
+  LOG_TIME datetime default NULL,
+  ADMIN_UID mediumint(8) unsigned NOT NULL default '0',
+  UID mediumint(8) unsigned NOT NULL default '0',
+  FID mediumint(8) unsigned NOT NULL default '0',
+  TID mediumint(8) unsigned NOT NULL default '0',
+  PID mediumint(8) unsigned NOT NULL default '0',
+  PSID mediumint(8) unsigned NOT NULL default '0',
+  PIID mediumint(8) unsigned NOT NULL default '0',
+  ACTION mediumint(8) unsigned NOT NULL default '0',
+  PRIMARY KEY  (LOG_ID)
+) TYPE=MyISAM;
+
+# --------------------------------------------------------
+
+#
 # Table structure for table `BANNED_IP`
 #
 
@@ -360,6 +380,7 @@ CREATE TABLE USER_PREFS (
   UID mediumint(8) unsigned default NULL,
   FIRSTNAME varchar(32) default NULL,
   LASTNAME varchar(32) default NULL,
+  DOB date default '0000-00-00' NULL,
   HOMEPAGE_URL varchar(255) default NULL,
   PIC_URL varchar(255) default NULL,
   EMAIL_NOTIFY char(1) default NULL,

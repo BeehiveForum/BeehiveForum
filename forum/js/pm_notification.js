c2a845803bd3ccc745ea07c5972fa977f475b1c5
Matt Beale <matt@beehiveforum.co.uk>
2003-08-24 17:29:57 +0000

    pm_notification.js

---
+++ forum/js/pm_notification.js
@@ -0,0 +1,29 @@
+/*======================================================================
+Copyright Project BeehiveForum 2002
+
+This file is part of BeehiveForum.
+
+BeehiveForum is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BeehiveForum is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Beehive; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+USA
+======================================================================*/
+
+/* $Id: pm_notification.js,v 1.1 2003-08-24 17:29:57 decoyduck Exp $ */
+
+function pm_notification() {
+    if (window.confirm('You have a new PM. Would you like to go to your Inbox now?')) {
+        top.frames['main'].location.replace('pm.php');
+    }
+    return true;
+}

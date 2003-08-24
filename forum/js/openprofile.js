c3d6c86d44b0270b0a1c62e3eb5ed142def392e7
HeyRatFans <matt@heyratfans.co.uk>
2003-08-24 16:46:24 +0000

    Beehive's first .js file.

---
+++ forum/js/openprofile.js
@@ -0,0 +1,26 @@
+/*======================================================================
+Copyright Project BeehiveForum 2002
+
+This file is part of BeehiveForum.
+
+BeehiveForum is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BeehiveForum is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Beehive; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+USA
+======================================================================*/
+
+/* $Id: openprofile.js,v 1.1 2003-08-24 16:46:24 decoyduck Exp $ */
+
+function openProfile(uid) {
+  window.open('user_profile.php?uid=' + uid, uid,'width=500, height=450, toolbars=no');
+}

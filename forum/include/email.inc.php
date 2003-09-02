d287e9f14133fd46007fcc4ddcc9f176860dff68
Matt Beale <matt@beehiveforum.co.uk>
2003-09-02 10:32:30 +0000

    Added Nickname to mail headers and also ran the from logon/nickname through format_user_name rather than just including the user's logon.

--- forum/include/email.inc.php
+++ forum/include/email.inc.php
@@ -21,12 +21,13 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: email.inc.php,v 1.33 2003-08-31 16:21:06 hodcroftcj Exp $ */
+/* $Id: email.inc.php,v 1.34 2003-09-02 10:32:30 decoyduck Exp $ */
 
 require_once("./include/db.inc.php"); // Database functions
 require_once("./include/format.inc.php"); // Formatting functions
 require_once("./include/config.inc.php"); // Formatting functions
 require_once("./include/constants.inc.php");
+require_once("./include/user.inc.php");
 
 function email_sendnotification($tuid, $msg, $fuid)
 {
@@ -36,11 +37,11 @@ function email_sendnotification($tuid, $msg, $fuid)
 
     $db_email_sendnotification = db_connect();
 
-    $sql = "select PREFS.EMAIL_NOTIFY, PROFILE.NICKNAME, PROFILE.EMAIL from ";
+    $sql = "SELECT PREFS.EMAIL_NOTIFY, PROFILE.NICKNAME, PROFILE.EMAIL FROM ";
     $sql.= forum_table("USER_PREFS") . " PREFS, ";
     $sql.= forum_table("USER") . " PROFILE ";
-    $sql.= "where PROFILE.UID = $tuid ";
-    $sql.= "and PROFILE.UID = PREFS.UID";
+    $sql.= "WHERE PROFILE.UID = $tuid ";
+    $sql.= "AND PROFILE.UID = PREFS.UID";
 
     $result = db_query($sql, $db_email_sendnotification);
 
@@ -50,15 +51,18 @@ function email_sendnotification($tuid, $msg, $fuid)
 
         if ($mailto['EMAIL_NOTIFY'] == 'Y' && $mailto['EMAIL'] != '') {
 
-            $sql = "select LOGON from ". forum_table("USER") . " where UID = $fuid";
+            $sql = "SELECT LOGON, NICKNAME FROM ". forum_table("USER") . " WHERE UID = $fuid";
             $resultfrom = db_query($sql, $db_email_sendnotification);
             $mailfrom = db_fetch_array($resultfrom);
 
             list($tid, $pid) = explode('.', $msg);
             $thread = thread_get($tid);
-            $lang = email_get_language($tuid); // get the right language for the email
-            
-            $message = strtoupper($mailfrom['LOGON']). " {$lang['msgnotificationemail_1']} $forum_name\n\n";
+
+             // get the right language for the email
+            $lang = email_get_language($tuid);
+
+            $message = format_user_name($mailfrom['LOGON'], $mailfrom['NICKNAME']);
+            $message.= " {$lang['msgnotificationemail_1']} $forum_name\n\n";
             $message.= "{$lang['msgnotificationemail_2']}:  ". _htmlentities_decode(_stripslashes($thread['TITLE'])). "\n\n";
             $message.= "{$lang['msgnotificationemail_3']}:\n";
             $message.= "http://". $HTTP_SERVER_VARS['HTTP_HOST'];
@@ -75,10 +79,16 @@ function email_sendnotification($tuid, $msg, $fuid)
 
             $header = "From: \"$forum_name\" <$forum_email>\n";
             $header.= "Reply-To: \"$forum_name\" <$forum_email>\n";
-            $header.= "Content-type: text/plain; charset=UTF-8\n";
+            $header.= "Content-type: text/plain; charset={$lang['_charset']}\n";
             $header.= "X-Mailer: PHP/". phpversion(). "\n";
 
-            mail($mailto['EMAIL'], "{$lang['msgnotification_subject']} $forum_name", $message, $header);
+            if (isset($mailto['NICKNAME']) && trim($mailto['NICKNAME']) != "") {
+                $recipient = "\"{$mailto['NICKNAME']}\" <{$mailto['EMAIL']}>";
+            }else {
+                $recipient = $mailto['EMAIL'];
+            }
+
+            mail($recipient, "{$lang['msgnotification_subject']} $forum_name", $message, $header);
 
         }
     }
@@ -97,13 +107,13 @@ function email_sendsubscription($tuid, $msg, $fuid)
 
     list($tid, $pid) = explode('.', $msg);
 
-    $sql = "select USER.UID, USER.NICKNAME, USER.EMAIL from ";
+    $sql = "SELECT USER.UID, USER.NICKNAME, USER.EMAIL FROM ";
     $sql.= forum_table("USER_THREAD") . " USER_THREAD, ";
     $sql.= forum_table("USER") . " USER ";
-    $sql.= "where USER_THREAD.TID = $tid ";
-    $sql.= "and USER_THREAD.INTEREST = 2 ";
-    $sql.= "and USER.UID = USER_THREAD.UID ";
-    $sql.= "and USER.UID <> $fuid and USER.UID <> $tuid";
+    $sql.= "WHERE USER_THREAD.TID = $tid ";
+    $sql.= "AND USER_THREAD.INTEREST = 2 ";
+    $sql.= "AND USER.UID = USER_THREAD.UID ";
+    $sql.= "AND USER.UID <> $fuid AND USER.UID <> $tuid";
 
     $result = db_query($sql, $db_email_sendsubscription);
     $numRows = db_num_rows($result);
@@ -112,13 +122,16 @@ function email_sendsubscription($tuid, $msg, $fuid)
 
         $mailto = db_fetch_array($result);
 
-        $sql = "select LOGON from ". forum_table("USER") . " where UID = $fuid";
+        $sql = "SELECT LOGON, NICKNAME FROM ". forum_table("USER") . " WHERE UID = $fuid";
         $resultfrom = db_query($sql, $db_email_sendsubscription);
         $mailfrom = db_fetch_array($resultfrom);
         $thread = thread_get($tid);
-        $lang = email_get_language($tuid); // get the right language for the email
 
-        $message = strtoupper($mailfrom['LOGON']). " {$lang['subnotification_1']}\n";
+        // get the right language for the email
+        $lang = email_get_language($tuid);
+
+        $message = format_user_name($mailfrom['LOGON'], $mailfrom['NICKNAME']);
+        $message.= " {$lang['subnotification_1']}\n";
         $message.= "{$lang['subnotification_2']} $forum_name\n\n";
         $message.= "{$lang['subnotification_3']}:  ". _htmlentities_decode(_stripslashes($thread['TITLE'])). "\n\n";
         $message.= "{$lang['subnotification_4']}:\n";
@@ -136,10 +149,16 @@ function email_sendsubscription($tuid, $msg, $fuid)
 
         $header = "From: \"$forum_name\" <$forum_email>\n";
         $header.= "Reply-To: \"$forum_name\" <$forum_email>\n";
-        $header.= "Content-type: text/plain; charset=UTF-8\n";
+        $header.= "Content-type: text/plain; charset={$lang['_charset']}\n";
         $header.= "X-Mailer: PHP/". phpversion(). "\n";
 
-        mail($mailto['EMAIL'], "{$lang['subnotification_subject']} $forum_name", $message, $header);
+        if (isset($mailto['NICKNAME']) && trim($mailto['NICKNAME']) != "") {
+            $recipient = "\"{$mailto['NICKNAME']}\" <{$mailto['EMAIL']}>";
+        }else {
+            $recipient = $mailto['EMAIL'];
+        }
+
+        mail($recipient, "{$lang['subnotification_subject']} $forum_name", $message, $header);
 
     }
 
@@ -151,7 +170,7 @@ function email_send_pm_notification($tuid, $mid, $fuid)
 {
     if (!(bool)ini_get('sendmail_from') || !(bool)ini_get('SMTP') || !(bool)ini_get('sendmail_path')) return false;
 
-    global $HTTP_SERVER_VARS, $forum_name, $forum_email;
+    global $HTTP_SERVER_VARS, $forum_name, $forum_email, $lang;
 
     $db_email_sendnotification = db_connect();
 
@@ -167,14 +186,17 @@ function email_send_pm_notification($tuid, $mid, $fuid)
 
         if ($mailto['PM_NOTIFY_EMAIL'] == 'Y' && $mailto['EMAIL'] != '') {
 
-            $sql = "select LOGON from ". forum_table("USER") . " where UID = $fuid";
+            $sql = "SELECT LOGON, NICKNAME FROM ". forum_table("USER") . " WHERE UID = $fuid";
             $resultfrom = db_query($sql, $db_email_sendnotification);
             $mailfrom = db_fetch_array($resultfrom);
 
             $pm_message = pm_single_get($mid, PM_FOLDER_INBOX, $tuid);
-            $lang = email_get_language($tuid); // get the right language for the email
 
-            $message = strtoupper($mailfrom['LOGON']). " {$lang['pmnotification_1']} $forum_name\n\n";
+             // get the right language for the email
+            $lang = email_get_language($tuid);
+
+            $message = format_user_name($mailfrom['LOGON'], $mailfrom['NICKNAME']);
+            $message.= " {$lang['pmnotification_1']} $forum_name\n\n";
             $message.= "{$lang['pmnotification_2']}:  ". _htmlentities_decode(_stripslashes($pm_message['SUBJECT'])). "\n\n";
             $message.= "{$lang['pmnotification_3']}:\n";
             $message.= "http://". $HTTP_SERVER_VARS['HTTP_HOST'];
@@ -191,10 +213,16 @@ function email_send_pm_notification($tuid, $mid, $fuid)
 
             $header = "From: \"$forum_name\" <$forum_email>\n";
             $header.= "Reply-To: \"$forum_name\" <$forum_email>\n";
-            $header.= "Content-type: text/plain; charset=UTF-8\n";
+            $header.= "Content-type: text/plain; charset={$lang['_charset']}\n";
             $header.= "X-Mailer: PHP/". phpversion(). "\n";
 
-            mail($mailto['EMAIL'], "{$lang['pmnotification_subject']} $forum_name", $message, $header);
+            if (isset($mailto['NICKNAME']) && trim($mailto['NICKNAME']) != "") {
+                $recipient = "\"{$mailto['NICKNAME']}\" <{$mailto['EMAIL']}>";
+            }else {
+                $recipient = $mailto['EMAIL'];
+            }
+
+            mail($recipient, "{$lang['pmnotification_subject']} $forum_name", $message, $header);
 
         }
     }
@@ -202,6 +230,8 @@ function email_send_pm_notification($tuid, $mid, $fuid)
     return true;
 
 }
+
+// Sends a password reminder email. Returns true on success, false on fail.
 
 function email_send_pw_reminder($logon)
 {
@@ -212,7 +242,7 @@ function email_send_pw_reminder($logon)
     $db_email_send_pw_reminder = db_connect();
     $logon = addslashes($logon);
 
-    $sql = "select UID, PASSWD, EMAIL from ". forum_table("USER") ." where LOGON '$logon'";
+    $sql = "SELECT UID, PASSWD, EMAIL FROM ". forum_table("USER") ." WHERE LOGON '$logon'";
     $result = db_query($sql, $db_email_send_pw_reminder);
 
     if (db_num_rows($result)) {
@@ -220,10 +250,11 @@ function email_send_pw_reminder($logon)
         $mailto = db_fetch_array($result);
 
 	if (isset($mailto['UID']) && isset($mailto['EMAIL']) && isset($mailto['PASSWD'])) {
-        
-            $lang = email_get_language($tuid); // get the right language for the email
 
-	        $message = "{$lang['forgotpwemail_1']} $forum_name {$lang['forgotpwemail_2']}\n\n";
+            // get the right language for the email
+            $lang = email_get_language($tuid);
+
+	    $message = "{$lang['forgotpwemail_1']} $forum_name {$lang['forgotpwemail_2']}\n\n";
             $message.= "{$lang['forgotpwemail_3']}:\n\n";
             $message.= "http://". $HTTP_SERVER_VARS['HTTP_HOST'];
 
@@ -235,32 +266,45 @@ function email_send_pw_reminder($logon)
 
             $header = "From: \"$forum_name\" <$forum_email>\n";
             $header.= "Reply-To: \"$forum_name\" <$forum_email>\n";
-	    $header.= "Content-type: text/plain; charset=UTF-8\n";
+            $header.= "Content-type: text/plain; charset={$lang['_charset']}\n";
             $header.= "X-Mailer: PHP/". phpversion();
 
-            if (mail($mailto['EMAIL'], "{$lang['passwdresetrequest']} - $forum_name", $msg, $header)) return true;
+            if (isset($mailto['NICKNAME']) && trim($mailto['NICKNAME']) != "") {
+                $recipient = "\"{$mailto['NICKNAME']}\" <{$mailto['EMAIL']}>";
+            }else {
+                $recipient = $mailto['EMAIL'];
+            }
+
+            if (mail($recipient, "{$lang['passwdresetrequest']} - $forum_name", $msg, $header)) return true;
 	}
     }
 
     return false;
 }
 
-function email_get_language($to_uid) // fetches the correct language file for the UID ($to_uid) who the email is being sent to
+// fetches the correct language file for the UID ($to_uid) who the email is being sent to
+
+function email_get_language($to_uid)
 {
-    require_once("./include/user.inc.php");
     global $default_language;
+
     $prefs = user_get_prefs($to_uid);
-    if (isset($prefs['LANGUAGE']) && $prefs['LANGUAGE'] != "") { // if the user has expressed a preference for language, use it if available
-        if (file_exists("./include/languages/".$prefs['LANGUAGE'].".inc.php")) {
-             require("./include/languages/".$prefs['LANGUAGE'].".inc.php");
-             return $lang; // $lang is the language array we've just included
-        } else {
-             require("./include/languages/".$default_language.".inc.php");
+
+     // if the user has expressed a preference for language, use it
+     // if available otherwise use the default language.
+
+    if (isset($prefs['LANGUAGE']) && trim($prefs['LANGUAGE']) != "") {
+        if (file_exists("./include/languages/{$prefs['LANGUAGE']}.inc.php")) {
+             require("./include/languages/{$prefs['LANGUAGE']}.inc.php");
+             return $lang;
+        }else {
+             require("./include/languages/{$default_language}.inc.php");
              return $lang;
         }
-    } else { // if the user has no language prefs, use the default language
-         require("./include/languages/".$default_language.".inc.php");
+    }else {
+         require("./include/languages/{$default_language}.inc.php");
          return $lang;
     }
 }
+
 ?>

0b501299b4b13e864cd5033339b3d562c4b8caaf
Matt Beale <matt@beehiveforum.co.uk>
2004-03-13 00:12:41 +0000

    Changed the version text in all the files to a constant that is stored in constants.inc.php. Makes it easier to change the version number now prior to a release.

--- forum/include/beehive.inc.php
+++ forum/include/beehive.inc.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: beehive.inc.php,v 1.34 2004-03-12 22:14:10 decoyduck Exp $ */
+/* $Id: beehive.inc.php,v 1.35 2004-03-13 00:12:41 decoyduck Exp $ */
 
 function draw_beehive_bar()
 {
@@ -31,7 +31,7 @@ function draw_beehive_bar()
     echo "<table width=\"96%\" class=\"posthead\">\n";
     echo "  <tr>\n";
     echo "    <td width=\"60%\" class=\"smalltext\" align=\"left\">\n";
-    echo "      Beehive Forum 0.4.1-dev8&nbsp;|&nbsp;\n";
+    echo "      Beehive Forum ", BEEHIVE_VERSION, "&nbsp;|&nbsp;\n";
     echo "      <a href=\"http://beehiveforum.net/faq\" target=\"_blank\">{$lang['faq']}</a>&nbsp;|&nbsp;\n";
     echo "      <a href=\"http://sourceforge.net/docman/?group_id=50772\" target=\"_blank\">{$lang['docs']}</a>&nbsp;|&nbsp;\n";
     echo "      <a href=\"http://sourceforge.net/tracker/?group_id=50772&amp;atid=460926\" target=\"_blank\">{$lang['support']}</a>\n";

7a853ba66d98ede899996bed41164a0d674dc387
HeyRatFans <matt@heyratfans.co.uk>
2004-04-11 20:13:46 +0000

    Fixed bug in nav.php prompting user to login but because the frame is so small you can't see the login form.

--- forum/nav.php
+++ forum/nav.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: nav.php,v 1.64 2004-04-10 16:35:00 decoyduck Exp $ */
+/* $Id: nav.php,v 1.65 2004-04-11 20:13:46 decoyduck Exp $ */
 
 // Compress the output
 include_once("./include/gzipenc.inc.php");
@@ -41,41 +41,7 @@ include_once("./include/logon.inc.php");
 include_once("./include/pm.inc.php");
 include_once("./include/session.inc.php");
 
-if (!$user_sess = bh_session_check()) {
-
-    if (isset($HTTP_SERVER_VARS["REQUEST_METHOD"]) && $HTTP_SERVER_VARS["REQUEST_METHOD"] == "POST") {
-        
-        if (perform_logon(false)) {
-	    
-	    html_draw_top();
-
-            echo "<h1>{$lang['loggedinsuccessfully']}</h1>";
-            echo "<div align=\"center\">\n";
-	    echo "<p><b>{$lang['presscontinuetoresend']}</b></p>\n";
-
-            $request_uri = get_request_uri();
-
-            echo "<form method=\"post\" action=\"$request_uri\" target=\"_self\">\n";
-
-            foreach($HTTP_POST_VARS as $key => $value) {
-	        form_input_hidden($key, _htmlentities(_stripslashes($value)));
-            }
-
-	    echo form_submit(md5(uniqid(rand())), $lang['continue']), "&nbsp;";
-            echo form_button(md5(uniqid(rand())), $lang['cancel'], "onclick=\"self.location.href='$request_uri'\""), "\n";
-	    echo "</form>\n";
-	    
-	    html_draw_bottom();
-	    exit;
-	}
-
-    }else {
-        html_draw_top();
-        draw_logon_form(false);
-	html_draw_bottom();
-	exit;
-    }
-}
+$user_sess = bh_session_check();
 
 // Fetch the forum webtag and settings
 $webtag = get_webtag();

11c07010403cafb06c527204cffa04d9d7e3e6e8
HeyRatFans <matt@heyratfans.co.uk>
2004-04-28 18:40:58 +0000

    start_main.php for the Default Forum

---
+++ forum/forums/default/start_main.php
@@ -0,0 +1,47 @@
+<!doctype HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Project Beehive</title>
+<style type="text/css">
+<!--
+
+.bodytext    { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 11px;
+               font-style: normal; line-height: 13px; font-weight: normal; color: #666666;
+               background-color: #EAEFF4 }
+
+.title       { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 18px;
+               font-style: normal; font-weight: bold; color: #ffffff; background-color: #A6BED7 }
+
+a            { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 11px;
+               line-height: 13px; font-weight: normal; color: #333399;
+               text-decoration: underline }
+
+-->
+</style>
+</head>
+
+<body class="bodytext">
+<table width="100%" border="0" cellspacing="0" cellpadding="8">
+  <tr>
+    <td class="title">Welcome to your new Beehive Forum!</td>
+  </tr>
+
+  <tr>
+    <td class="bodytext"><a href="http://sourceforge.net/projects/beehiveforum/" target="_blank">Home</a> | <a href="http://beehiveforum.net/faq">FAQ</a> | <a href="http://sourceforge.net/docman/?group_id=50772" target="_blank">Docs</a> | <a href="http://sourceforge.net/project/showfiles.php?group_id=50772" target="_blank"> Download</a> | <a href="../forums.php">Live Forums</a></td>
+
+  </tr>
+  <tr>
+    <td height="1" class="title"></td>
+  </tr>
+  <tr>
+    <td valign="top" class="bodytext">
+      <p>You can modify this start page from the admin interface.</p>
+    </td>
+
+  </tr>
+  <tr>
+    <td height="1" class="title"> </td>
+  </tr>
+</table>
+</body>
+</html>

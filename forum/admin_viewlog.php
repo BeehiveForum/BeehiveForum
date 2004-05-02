36c51e290e5337ead27fc62473e0e7605d105aaf
Matt Beale <matt@beehiveforum.co.uk>
2004-05-02 15:28:38 +0000

    Wrong items per page count on admin_viewlog.php

--- forum/admin_viewlog.php
+++ forum/admin_viewlog.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: admin_viewlog.php,v 1.59 2004-04-29 21:01:06 decoyduck Exp $ */
+/* $Id: admin_viewlog.php,v 1.60 2004-05-02 15:28:38 decoyduck Exp $ */
 
 // Compress the output
 include_once("./include/gzipenc.inc.php");
@@ -365,7 +365,7 @@ echo "    <tr>\n";
 echo "      <td>&nbsp;</td>";
 echo "    </tr>\n";
 echo "    <tr>\n";
-echo "      <td class=\"postbody\" align=\"center\">{$lang['pages']}: ", page_links(get_request_uri(), $start, $admin_log_array['admin_log_count'], 10), "</td>\n";
+echo "      <td class=\"postbody\" align=\"center\">{$lang['pages']}: ", page_links(get_request_uri(), $start, $admin_log_array['admin_log_count'], 20), "</td>\n";
 echo "    </tr>\n";
 echo "    <tr>\n";
 echo "      <td>&nbsp;</td>\n";

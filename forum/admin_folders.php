0c9acf06190bef4a221072995a52988260b40d4c
Matt Beale <matt@beehiveforum.co.uk>
2004-05-04 23:04:22 +0000

    Some changes to the admin_folders page. Looks less professional at the moment, but it's no where near as cluttered. It doesn't do owt at the moment by the way but it's gone midnight and I'm knackered.

--- forum/admin_folders.php
+++ forum/admin_folders.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: admin_folders.php,v 1.72 2004-05-04 17:10:16 decoyduck Exp $ */
+/* $Id: admin_folders.php,v 1.73 2004-05-04 23:04:22 decoyduck Exp $ */
 
 // Compress the output
 include_once("./include/gzipenc.inc.php");
@@ -106,6 +106,22 @@ if (!(bh_session_get_value('STATUS')&USER_PERM_SOLDIER)) {
     exit;
 }
 
+if (isset($_POST['t_more'])) {
+
+    list($fid) = array_keys($_POST['t_more']);
+    header_redirect("./admin_folder_edit.php?webtag=$webtag&fid=$fid");
+}
+
+if (isset($_POST['addnew'])) {
+    header_redirect("./admin_folder_Add.php?webtag=$webtag");
+}
+
+if (isset($_POST['t_permissions'])) {
+
+    list($fid) = array_keys($_POST['t_permissions']);
+    header_redirect("./admin_folder_access.php?webtag=$webtag&fid=$fid");
+}
+
 // Do updates
 if (isset($_POST['submit'])) {
 
@@ -113,34 +129,19 @@ if (isset($_POST['submit'])) {
 
         foreach($_POST['t_fid'] as $fid => $value) {
 
-            $new_title  = trim(_stripslashes($_POST['t_title'][$fid]));
-            $new_desc   = trim(_stripslashes($_POST['t_desc'][$fid]));
+            $folder_data = folder_get($fid);
 
-	    $new_access = (is_numeric($_POST['t_access'][$fid]))   ? $_POST['t_access'][$fid]   : 0;
-            $new_allow  = (is_numeric($_POST['t_allow'][$fid]))    ? $_POST['t_allow'][$fid]    : 0;
-	    $new_pos    = (is_numeric($_POST['t_position'][$fid])) ? $_POST['t_position'][$fid] : 0;
-
-            folder_update($_POST['t_fid'][$fid], $new_title, $new_access, $new_desc, $new_allow, $new_pos);
-            admin_addlog(0, $_POST['t_fid'][$fid], 0, 0, 0, 0, 7);
-
-            if ($_POST['t_fid'][$fid] != $_POST['t_move'][$fid]) {
-                folder_move_threads($_POST['t_fid'][$fid], $_POST['t_move'][$fid]);
-                admin_addlog(0, $_POST['t_fid'][$fid], 0, 0, 0, 0, 8);
+            if (isset($_POST['t_position'][$fid]) && is_numeric($_POST['t_position'][$fid])) {
+                $folder_data['POSITION'] = $_POST['t_position'][$fid];
             }
+
+            if (isset($_POST['t_access'][$fid]) && is_numeric($_POST['t_access'][$fid])) {
+                $folder_data['ACCESS_LEVEL'] = $_POST['t_access'][$fid];
+            }
+
+            folder_update($fid, $folder_data);
+            admin_addlog(0, $fid, 0, 0, 0, 0, 7);
         }
-    }
-
-    if (strlen(trim(_stripslashes($_POST['t_title_new']))) > 0 && trim(_stripslashes($_POST['t_title_new'])) != $lang['newfolder']) {
-
-        $new_title  = trim(_stripslashes($_POST['t_title_new']));
-	$new_desc   = trim(_stripslashes($_POST['t_desc_new']));
-        $new_access = (is_numeric($_POST['t_access_new'])) ? $_POST['t_access_new'] : 0;
-	$new_allow  = (is_numeric($_POST['t_allow_new'])) ? $_POST['t_allow_new'] : 0;
-	$new_pos    = (isset($_POST['t_fid'])) ? sizeof($_POST['t_fid']) : 1;
-
-        $new_fid = folder_create($new_title, $new_access, $new_desc, $new_allow, $new_pos );
-        admin_addlog(0, $new_fid, 0, 0, 0, 0, 9);
-
     }
 }
 
@@ -150,7 +151,7 @@ echo "<br />\n";
 echo "<div align=\"center\">\n";
 echo "<form name=\"f_folders\" action=\"admin_folders.php\" method=\"post\">\n";
 echo "  ", form_input_hidden('webtag', $webtag), "\n";
-echo "  <table cellpadding=\"0\" cellspacing=\"0\" width=\"96%\">\n";
+echo "  <table cellpadding=\"0\" cellspacing=\"0\" width=\"70%\">\n";
 echo "    <tr>\n";
 echo "      <td>\n";
 echo "        <table class=\"box\" width=\"100%\">\n";
@@ -160,20 +161,14 @@ echo "              <table class=\"posthead\" width=\"100%\">\n";
 echo "                <tr>\n";
 echo "                  <td class=\"subhead\" align=\"left\" nowrap=\"nowrap\">&nbsp;{$lang['position']}</td>\n";
 echo "                  <td class=\"subhead\" align=\"left\" nowrap=\"nowrap\">&nbsp;{$lang['foldername']}</td>\n";
-echo "                  <td class=\"subhead\" align=\"left\" nowrap=\"nowrap\">&nbsp;{$lang['description']}</td>\n";
 echo "                  <td class=\"subhead\" align=\"left\" nowrap=\"nowrap\">&nbsp;{$lang['accesslevel']}</td>\n";
 echo "                  <td class=\"subhead\" align=\"left\" nowrap=\"nowrap\">&nbsp;{$lang['threadcount']}</td>\n";
-echo "                  <td class=\"subhead\" align=\"left\" nowrap=\"nowrap\">&nbsp;{$lang['move']}</td>\n";
-echo "                  <td class=\"subhead\" align=\"left\" nowrap=\"nowrap\">&nbsp;{$lang['allow']}</td>\n";
+echo "                  <td class=\"subhead\" align=\"left\" nowrap=\"nowrap\">&nbsp;{$lang['more']}</td>\n";
 echo "                  <td class=\"subhead\" align=\"left\" nowrap=\"nowrap\">&nbsp;{$lang['permissions']}</td>\n";
+echo "                  <td class=\"subhead\" align=\"left\" nowrap=\"nowrap\">&nbsp;{$lang['delete']}</td>\n";
 echo "                </tr>\n";
 
 $folder_array = folder_get_all();
-
-// Make the arrays for the allow post types dropdown
-
-$allow_labels = array($lang['normalthreadsonly'], $lang['pollthreadsonly'], $lang['both']);
-$allow_values = array(FOLDER_ALLOW_NORMAL_THREAD, FOLDER_ALLOW_POLL_THREAD, FOLDER_ALLOW_ALL_THREAD);
 
 if ($folder_array = folder_get_all()) {
 
@@ -184,38 +179,31 @@ if ($folder_array = folder_get_all()) {
 
         echo "                <tr>\n";
         echo "                  <td align=\"left\">", form_dropdown_array("t_position[{$folder_array[$i]['FID']}]", range(1, sizeof($folder_array) + 1), range(1, sizeof($folder_array) + 1), $i + 1), form_input_hidden("t_old_position[{$folder_array[$i]['FID']}]", $i), form_input_hidden("t_fid[{$folder_array[$i]['FID']}]", $folder_array[$i]['FID']), "</td>\n";
-        echo "                  <td align=\"left\">". form_field("t_title[{$folder_array[$i]['FID']}]", $folder_array[$i]['TITLE'], 25, 32). form_input_hidden("t_old_title[{$folder_array[$i]['FID']}]", $folder_array[$i]['TITLE']). "</td>\n";
-        echo "                  <td align=\"left\">". form_field("t_desc[{$folder_array[$i]['FID']}]", $folder_array[$i]['DESCRIPTION'], 32, 255). form_input_hidden("t_old_desc[{$folder_array[$i]['FID']}]", $folder_array[$i]['DESCRIPTION']). "</td>\n";
+        echo "                  <td align=\"left\"><a href=\"admin_folder_edit.php?webtag=$webtag&amp;fid={$folder_array[$i]['FID']}\">{$folder_array[$i]['TITLE']}</a></td>\n";
 
         // Draw the ACCESS_LEVEL dropdown
         echo "                  <td align=\"left\">".form_dropdown_array("t_access[{$folder_array[$i]['FID']}]", array(-1, 0, 1, 2), array($lang['closed'], $lang['open'], $lang['restricted'], $lang['locked']), $folder_array[$i]['ACCESS_LEVEL']);
         echo form_input_hidden("t_old_access[{$folder_array[$i]['FID']}]", $folder_array[$i]['ACCESS_LEVEL']). "</td>\n";
 
         echo "                  <td align=\"left\">". $folder_array[$i]['THREAD_COUNT']. "</td>\n";
-        echo "                  <td align=\"left\">". folder_draw_dropdown($folder_array[$i]['FID'], "t_move", "[{$folder_array[$i]['FID']}]"). "</td>\n";
-        echo "                  <td align=\"left\">". form_dropdown_array("t_allow[{$folder_array[$i]['FID']}]", $allow_values, $allow_labels, $folder_array[$i]['ALLOWED_TYPES'] ? $folder_array[$i]['ALLOWED_TYPES'] : FOLDER_ALLOW_NORMAL_THREAD | FOLDER_ALLOW_POLL_THREAD).form_input_hidden("t_old_allow[{$folder_array[$i]['FID']}]", $folder_array[$i]['ALLOWED_TYPES'])."</td>\n";
+        echo "                  <td align=\"left\">", form_submit("t_more[{$folder_array[$i]['FID']}]", $lang['more']), "</td>\n";
 
         if ($folder_array[$i]['ACCESS_LEVEL'] > 0) {
-            echo "                  <td align=\"left\">", form_button("permissions", $lang['change'], "onclick=\"document.location.href='admin_folder_access.php?webtag=$webtag&amp;fid={$folder_array[$i]['FID']}'\""), "</td>\n";
+            echo "                  <td align=\"left\" width=\"100\">", form_submit("t_permissions[{$folder_array[$i]['FID']}]", $lang['change']), "</td>\n";
         }else {
-            echo "                  <td align=\"left\">&nbsp;</td>\n";
+            echo "                  <td align=\"left\" width=\"100\">&nbsp;</td>\n";
+        }
+
+        if ($folder_array[$i]['THREAD_COUNT'] < 1) {
+            echo "                  <td align=\"left\" width=\"100\">", form_submit("t_delete[{$folder_array[$i]['FID']}]", $lang['delete']), "</td>\n";
+        }else {
+            echo "                  <td align=\"left\" width=\"100\">&nbsp;</td>\n";
         }
 
         echo "                </tr>\n";
     }
 }
 
-// Draw a row for a new folder to be created
-echo "                <tr>\n";
-echo "                  <td align=\"left\">{$lang['newcaps']}</td>\n";
-echo "                  <td align=\"left\">". form_field("t_title_new", $lang['newfolder'], 25, 32). "</td>\n";
-echo "                  <td align=\"left\">". form_field("t_desc_new", "", 32, 255). "</td>\n";
-echo "                  <td align=\"left\">". form_dropdown_array("t_access_new", array(-1, 0, 1, 2), array($lang['closed'], $lang['open'], $lang['restricted'], $lang['locked']), 0). "</td>\n";
-echo "                  <td align=\"left\">&nbsp;</td>\n";
-echo "                  <td align=\"left\">&nbsp;</td>\n";
-echo "                  <td align=\"left\">".form_dropdown_array("t_allow_new", $allow_values, $allow_labels, FOLDER_ALLOW_ALL_THREAD)."</td>\n";
-echo "                  <td align=\"left\">&nbsp;</td>\n";
-echo "                </tr>\n";
 echo "                <tr>\n";
 echo "                  <td colspan=\"8\">&nbsp;</td>\n";
 echo "                </tr>\n";
@@ -229,7 +217,7 @@ echo "    <tr>\n";
 echo "      <td>&nbsp;</td>\n";
 echo "    </tr>\n";
 echo "    <tr>\n";
-echo "      <td align=\"center\">", form_submit("submit", $lang['save']), "</td>\n";
+echo "      <td align=\"center\">", form_submit("submit", $lang['save']), "&nbsp;", form_submit("addnew", $lang['addnewfolder']), "</td>\n";
 echo "    </tr>\n";
 echo "  </table>\n";
 echo "</form>\n";;

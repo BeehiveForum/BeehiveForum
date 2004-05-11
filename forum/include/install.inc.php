6549dbf92d17ca968d99f5276df6f53a4c4e89be
HeyRatFans <matt@heyratfans.co.uk>
2004-05-11 17:32:28 +0000

    Prevented the install check from redirecting the user away from the current page so they can refresh it once they have removed the install folder.

--- forum/include/install.inc.php
+++ forum/include/install.inc.php
@@ -21,18 +21,54 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: install.inc.php,v 1.5 2004-05-10 15:56:49 decoyduck Exp $ */
+/* $Id: install.inc.php,v 1.6 2004-05-11 17:32:28 decoyduck Exp $ */
 
 if (@file_exists("./include/config.inc.php")) {
     include_once("./include/config.inc.php");
 }
 
-include_once("./include/header.inc.php");
+include_once("./include/html.inc.php");
 
 function check_install()
 {
-    if (file_exists('install') && !defined("BEEHIVE_INSTALL_NOWARN")) {
+    if (!defined("BEEHIVE_INSTALLED")) {
         header_redirect("./install/install.php");
+    }
+
+    if (file_exists('install') && !defined("BEEHIVE_INSTALL_NOWARN")) {
+
+        html_draw_top();
+
+        echo "<h1>BeehiveForum Installation Error</h1>\n";
+        echo "<br />\n";
+        echo "<div align=\"center\">\n";
+        echo "  <table cellpadding=\"0\" cellspacing=\"0\" width=\"400\">\n";
+        echo "    <tr>\n";
+        echo "      <td>\n";
+        echo "        <table class=\"box\">\n";
+        echo "          <tr>\n";
+        echo "            <td class=\"posthead\">\n";
+        echo "              <table class=\"posthead\" width=\"500\">\n";
+        echo "                <tr>\n";
+        echo "                  <td colspan=\"2\" class=\"subhead\">Installation Incomplete</td>\n";
+        echo "                </tr>\n";
+        echo "                <tr>\n";
+        echo "                  <td>Your BeehiveForum would appear to be already installed, but you have not removed the install folder. You must delete the 'install' directory before your Beehive Forum can be used.</td>\n";
+        echo "                </tr>\n";
+        echo "                <tr>\n";
+        echo "                  <td>&nbsp;</td>\n";
+        echo "                </tr>\n";
+        echo "              </table>\n";
+        echo "            </td>\n";
+        echo "          </tr>\n";
+        echo "        </table>\n";
+        echo "      </td>\n";
+        echo "    </tr>\n";
+        echo "  </table>\n";
+        echo "</div>\n";
+
+        html_draw_bottom();
+        exit;
     }
 }
 

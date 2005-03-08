9fc1f584806df00abba034ee2ec60c8276d5f13f
Matt Beale <matt@beehiveforum.co.uk>
2005-03-08 16:52:55 +0000

    Fixed bugs in various permisions functions. Don't think there are any more, but y'never know. Makes my head hurt getting around all the permissions tables and functions.

--- forum/admin_user_groups_add.php
+++ forum/admin_user_groups_add.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: admin_user_groups_add.php,v 1.16 2005-02-04 19:35:35 decoyduck Exp $ */
+/* $Id: admin_user_groups_add.php,v 1.17 2005-03-08 16:52:50 decoyduck Exp $ */
 
 // Compress the output
 include_once("./include/gzipenc.inc.php");
@@ -121,12 +121,13 @@ if (isset($_POST['submit'])) {
         $t_description = "";
     }
 
-    $t_admintools = (isset($_POST['t_admintools'])) ? $_POST['t_admintools'] : 0;
-    $t_banned     = (isset($_POST['t_banned']))     ? $_POST['t_banned']     : 0;
-    $t_wormed     = (isset($_POST['t_wormed']))     ? $_POST['t_wormed']     : 0;
-    $t_globalmod  = (isset($_POST['t_globalmod']))  ? $_POST['t_globalmod']  : 0;
+    $t_admintools = (double) (isset($_POST['t_admintools'])) ? $_POST['t_admintools'] : 0;
+    $t_banned     = (double) (isset($_POST['t_banned']))     ? $_POST['t_banned']     : 0;
+    $t_wormed     = (double) (isset($_POST['t_wormed']))     ? $_POST['t_wormed']     : 0;
+    $t_globalmod  = (double) (isset($_POST['t_globalmod']))  ? $_POST['t_globalmod']  : 0;
+    $t_linksmod   = (double) (isset($_POST['t_linksmod']))   ? $_POST['t_linksmod']   : 0;
 
-    $new_group_perms = (double) $t_banned | $t_wormed | $t_globalmod;
+    $new_group_perms = (double) ($t_banned | $t_wormed | $t_globalmod | $t_linksmod);
 
     if (perm_has_forumtools_access()) {
 
@@ -218,13 +219,16 @@ if (perm_has_forumtools_access()) {
 }
 
 echo "                      <tr>\n";
+echo "                        <td>", form_checkbox("t_globalmod", USER_PERM_FOLDER_MODERATE, $lang['usercanmoderateallfolders'], false), "</td>\n";
+echo "                      </tr>\n";
+echo "                      <tr>\n";
+echo "                        <td>", form_checkbox("t_linksmod", USER_PERM_LINKS_MODERATE, $lang['usercanmoderatelinkssection'], false), "</td>\n";
+echo "                      </tr>\n";
+echo "                      <tr>\n";
 echo "                        <td>", form_checkbox("t_banned", USER_PERM_BANNED, $lang['groupisbanned'], false), "</td>\n";
 echo "                      </tr>\n";
 echo "                      <tr>\n";
 echo "                        <td>", form_checkbox("t_wormed", USER_PERM_WORMED, $lang['groupiswormed'], false), "</td>\n";
-echo "                      </tr>\n";
-echo "                      <tr>\n";
-echo "                        <td>", form_checkbox("t_globalmod", USER_PERM_FOLDER_MODERATE, $lang['groupisglobalmod'], false), "</td>\n";
 echo "                      </tr>\n";
 echo "                      <tr>\n";
 echo "                        <td>&nbsp;</td>\n";

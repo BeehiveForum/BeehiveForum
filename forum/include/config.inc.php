9b270cf4524d82338c0bce12863729ebdb327cc7
Matt Beale <matt@beehiveforum.co.uk>
2005-04-19 23:45:23 +0000

    Fixing primary key row duplication error on RSS_HISTORY table.

--- forum/include/config.inc.php
+++ forum/include/config.inc.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: config.inc.php,v 1.125 2005-04-19 23:34:58 decoyduck Exp $ */
+/* $Id: config.inc.php,v 1.126 2005-04-19 23:45:22 decoyduck Exp $ */
 
 // MAIN CONFIGURATION FILE
 

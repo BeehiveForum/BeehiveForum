f6f541a8153bc67eaf11b56a8f552a33c0476939
Rowan Hill <no-reply@beehiveforum.co.uk>
2005-09-01 13:47:21 +0000

    Removed 'Reply to All' link from closed threads. (Except for mods)

--- forum/messages.php
+++ forum/messages.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: messages.php,v 1.181 2005-08-31 14:55:18 decoyduck Exp $ */
+/* $Id: messages.php,v 1.182 2005-09-01 13:47:21 rowan_hill Exp $ */
 
 /**
 * Displays a thread and processes poll votes
@@ -344,13 +344,19 @@ if ($msg_count > 0 && bh_session_get_value('UID') != 0 && !isset($_GET['markasre
     messages_update_read($tid, $last_pid, bh_session_get_value('UID'));
 }
 
+//print_r($threaddata);
+
 echo "<div align=\"center\">\n";
 echo "<table width=\"96%\" border=\"0\">\n";
 echo "  <tr>\n";
 echo "    <td colspan=\"3\">&nbsp;</td>\n";
 echo "  </tr>\n";
 echo "  <tr valign=\"top\">\n";
-echo "    <td width=\"33%\" align=\"left\"><p><img src=\"". style_image('reply_all.png') ."\" alt=\"{$lang['replyall']}\" title=\"{$lang['replyall']}\" border=\"0\" /> <a href=\"post.php?webtag=$webtag&amp;replyto=$tid.0\" target=\"_parent\"><b>{$lang['replyall']}</b></a></p></td>\n";
+if (($threaddata['CLOSED'] == 0) || perm_is_moderator($threaddata['FID'], bh_session_get_value('UID'))) {
+    echo "    <td width=\"33%\" align=\"left\"><p><img src=\"". style_image('reply_all.png') ."\" alt=\"{$lang['replyall']}\" title=\"{$lang['replyall']}\" border=\"0\" /> <a href=\"post.php?webtag=$webtag&amp;replyto=$tid.0\" target=\"_parent\"><b>{$lang['replyall']}</b></a></p></td>\n";
+} else {
+    echo "    <td width=\"33%\" align=\"left\">&nbsp;</td>\n";
+}
 
 echo "    <td width=\"33%\" align=\"center\"><p>";
 

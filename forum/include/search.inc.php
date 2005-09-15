1e990eea9eb1366e251430cc3bb6361f052b0024
Matt Beale <matt@beehiveforum.co.uk>
2005-09-15 18:50:57 +0000

    Changed: Finding an old post to index is more friendly to the server hopefully. Fixed: edit_attachments.php was showing "Other attachments" in My Controls when it shouldn't.

--- forum/include/search.inc.php
+++ forum/include/search.inc.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: search.inc.php,v 1.137 2005-09-13 14:04:51 decoyduck Exp $ */
+/* $Id: search.inc.php,v 1.138 2005-09-15 18:50:57 decoyduck Exp $ */
 
 // We shouldn't be accessing this file directly.
 
@@ -626,25 +626,27 @@ function search_index_old_post()
 
     $forum_fid = $table_data['FID'];
 
-    $sql = "SELECT POST.TID, POST.PID, POST.FROM_UID, POST.TO_UID, ";
-    $sql.= "UNIX_TIMESTAMP(POST.CREATED) AS CREATED FROM {$table_data['PREFIX']}POST POST ";
-    $sql.= "LEFT JOIN SEARCH_POSTS SEARCH_POSTS ON (SEARCH_POSTS.TID = POST.TID AND ";
-    $sql.= "SEARCH_POSTS.PID = POST.PID AND SEARCH_POSTS.FORUM = $forum_fid) ";
+    $sql = "SELECT POST.TID, POST.PID FROM {$table_data['PREFIX']}POST POST ";
+    $sql.= "LEFT JOIN SEARCH_POSTS SEARCH_POSTS ON ( SEARCH_POSTS.TID = POST.TID ";
+    $sql.= "AND SEARCH_POSTS.PID = POST.PID AND SEARCH_POSTS.FORUM = 1) ";
     $sql.= "WHERE SEARCH_POSTS.TID IS NULL AND SEARCH_POSTS.PID IS NULL ";
-    $sql.= "LIMIT 0, 1";
+    $sql.= "LIMIT 0 , 1";
 
     $result = db_query($sql, $db_search_index_old_post);
 
     if (db_num_rows($result) > 0) {
 
-        list($tid, $pid, $fuid, $tuid, $created) = db_fetch_array($result, DB_RESULT_NUM);
+        list($tid, $pid) = db_fetch_array($result, DB_RESULT_NUM);
+
+        $message = messages_get($tid, $pid, 1);
+        $message['CONTENT'] = message_get_content($tid, $pid);
 
         if (!$fid = thread_get_folder($tid)) $fid = 0;
         if (!$by_uid = thread_get_by_uid($tid)) $by_uid = 0;
 
-        $content = message_get_content($tid, $pid);
-
-        return search_index_post($fid, $tid, $pid, $by_uid, $fuid, $tuid, $content, $created);
+        return search_index_post($fid, $tid, $pid, $by_uid,
+                                 $message['FROM_UID'], $message['TO_UID'],
+                                 $message['CONTENT'], $message['CREATED']);
     }
 
     return false;

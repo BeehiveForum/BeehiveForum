6d64743ac54bb235ccacf35c3683b6ea695e9ca0
Matt Beale <matt@beehiveforum.co.uk>
2006-03-13 21:51:21 +0000

    Added: Filename to end of get_attachment.php links to try and spoof browsers who ignore the filename= header. Added: Logging to the DB functions. ooh and eak at the number of queries at messages.php runs!!

--- bh_cvs_log_parse.php
+++ bh_cvs_log_parse.php
@@ -21,66 +21,203 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: bh_cvs_log_parse.php,v 1.1 2006-02-12 17:11:09 decoyduck Exp $ */
+/* $Id: bh_cvs_log_parse.php,v 1.2 2006-03-13 21:51:19 decoyduck Exp $ */
 
-//  To use this you need to perform a dump of the CVS log and save it
-//  as log.txt. Commands to dump the CVS log is:
+// To use this you need to perform a dump of the CVS log and output
+// a correctly formatted change log for use on SF or within the
+// release.txt file.
 //
-//  From within /forum:
+// For this to work you need to have correctly set up SSH and CVS and
+// have created a ssh key otherwise you will be prompted for your
+// password for every call to CVS LOG, in excess of 20 times which
+// won't be fun at all.
 //
-//  > cvs log -N -S -d ">2005-01-01" >log.txt
-//
-//  You will also need the old log from the release.txt in the docs folder
-//  to remove any entries which may have been dated incorrectly in CVS.
-//  Copy and paste the lines from release.txt, remove the line wrapping
-//  (Regular expression match: [\n +]) and save it as old_log.txt.
-//
-//  Before running the log dump you should delete the folders you don't
-//  want to be included in the log, such as the Geshi and TinyMCE folders.
-//  Just make sure you have checked in all your changes before you delete them!
+// And no this won't work with TortoiseCVS and Pageant.
 
+// Constant to define where the include files are
+define("BH_INCLUDE_PATH", "./forum/include/");
 
-$log_array = file('log.txt');
-$old_log_array = file('old_log.txt');
+// Installation checking functions
+include_once(BH_INCLUDE_PATH. "install.inc.php");
 
-foreach($log_array as $log_entry) {
+function get_cvs_log_data($dir, $date)
+{
+    $cwd = getcwd();
 
-    if (preg_match("/^fixed:/i", $log_entry) > 0) {
-        $fixed_array[] = $log_entry;
+    if (dir_exists($dir)) {
+       
+        chdir($dir);
+        
+        if (@$log_handle = popen("cvs log -l -N -d \">$date\" 2>&1", 'r')) {
+
+            $log_contents = "";
+            
+            while(!feof($log_handle)) {
+                $log_contents.= fgets($log_handle);
+            }
+
+            pclose($log_handle);
+            chdir($cwd);
+            return $log_contents;
+        }
     }
 
-    if (preg_match("/^added:/i", $log_entry) > 0) {
-        $added_array[] = $log_entry;
-    }
-
-    if (preg_match("/^changed:/i", $log_entry) > 0) {
-        $changed_array[] = $log_entry;
-    }
-
-    if (preg_match("/^removed:/i", $log_entry) > 0) {
-        $removed_array[] = $log_entry;
-    }
+    chdir($cwd);
+    return false;
 }
 
-$fixed_array = array_unique($fixed_array);
-$added_array = array_unique($added_array);
-$changed_array = array_unique($changed_array);
-$removed_array = array_unique($removed_array);
+function get_cvs_dirs($path, &$cvs_dir_array)
+{
+    global $exclude_dirs;
+    
+    if (!is_array($cvs_dir_array)) {
+        $cvs_dir_array = array();
+    }
+    
+    if ($dir = opendir($path)) {
 
-$log_array = array_merge($fixed_array, $added_array, $changed_array, $removed_array);
+        while (($file = readdir($dir)) !== false) {
 
-foreach($log_array as $log_line => $log_entry) {
+            if ($file != "." && $file != ".." && $file != "CVS") {
+            
+                $grep_match = preg_quote("$path/$file", "/");
+                
+                if (is_dir("$path/$file")) {
 
-    if (!in_array($log_entry, $old_log_array)) {
+                    if (!preg_grep("/^$grep_match$/", $exclude_dirs)) {
+                        $cvs_dir_array[] = "$path/$file";
+                    }
 
-        if (preg_match("/^([a-z]+: )/i", $log_entry, $matches) > 0) {
-            $indent = str_repeat(" ", strlen($matches[1]) + 10);
-        }else {
-            $indent = str_repeat(" ", 18);
+                    get_cvs_dirs("$path/$file", $cvs_dir_array);
+                }
+            }
         }
 
-        echo "        - ", wordwrap(trim($log_entry), 75 - strlen($matches[1]), "\n$indent", 0), "\n";
+        closedir($dir);
     }
 }
+
+// Fetch our exclude list. This allows us to ignore some
+// BH addons which are included in CVS (Geshi and TinyMCE)
+
+$exclude_dirs = array();
+
+if (file_exists('bh_cvs_log_parse_exclude.php')) {
+    include('bh_cvs_log_parse_exclude.php');
+}
+
+// Check to see if we have a date on the command line and
+// that it is in the valid format YYYY-MM-DD. If we don't
+// then we need to bail out.
+
+if (isset($_SERVER['argv'][1]) && preg_match("/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/", $_SERVER['argv'][1]) > 0) {
+    
+    $date_from = $_SERVER['argv'][1];
+
+    // Get a list of directories to use
+
+    get_cvs_dirs('.', $cvs_log_folders);
+
+    // What we're actually going to be doing :)
+
+    echo "Please wait getting log entries.";
+
+    // Fetch the CVS log for each folder. At the moment this is done
+    // by looping through the array above as there doesn't appear to
+    // be a way log the entire repository while ignoring specific
+    // folders like the TinyMCE and Geshi folders.
+
+    foreach ($cvs_log_folders as $cvs_log_folder) {
+
+        if (!isset($cvs_log)) $cvs_log = "";
+        $cvs_log.= get_cvs_log_data($cvs_log_folder, $date_from);
+        echo ".";
+    }
+
+    // Got the log data, split it into an array by lines.
+
+    $log_array = explode("\n", $cvs_log);
+
+    // Optionally load the old log otherwise create an
+    // empty array.
+
+    if (!@$old_log_array = file('old_log.txt')) {
+        $old_log_array = array();
+    }
+
+    // Create the arrays. Prevents errors later if the
+    // log only contains added entries for example.
+
+    $fixed_array = array();
+    $added_array = array();
+    $changed_array = array();
+    $removed_array = array();
+
+    // Process the log entries by their prefix.
+
+    foreach($log_array as $log_entry) {
+
+        if (preg_match("/^fixed:/i", $log_entry) > 0) {
+            $fixed_array[] = $log_entry;
+        }
+
+        if (preg_match("/^added:/i", $log_entry) > 0) {
+            $added_array[] = $log_entry;
+        }
+
+        if (preg_match("/^changed:/i", $log_entry) > 0) {
+            $changed_array[] = $log_entry;
+        }
+
+        if (preg_match("/^removed:/i", $log_entry) > 0) {
+            $removed_array[] = $log_entry;
+        }
+    }
+
+    // Remove duplicate entries which are caused by
+    // checking in multiple files at a time.
+
+    $fixed_array = array_unique($fixed_array);
+    $added_array = array_unique($added_array);
+    $changed_array = array_unique($changed_array);
+    $removed_array = array_unique($removed_array);
+
+    // Merge the log data together into one array
+
+    $log_array = array_merge($fixed_array, $added_array, $changed_array, $removed_array);
+
+    // Loop through the array and indent the line correctly.
+    // If the log entry exists in the old log then we ignore it.
+
+    ob_start();
+
+    foreach($log_array as $log_line => $log_entry) {
+
+        if (!in_array($log_entry, $old_log_array)) {
+
+            if (preg_match("/^([a-z]+: )/i", $log_entry, $matches) > 0) {
+                $indent = str_repeat(" ", strlen($matches[1]) + 10);
+            }else {
+                $indent = str_repeat(" ", 18);
+            }
+
+            echo "        - ", wordwrap(trim($log_entry), 75 - strlen($indent), "\n$indent", 0), "\n";
+        }
+    }
+
+    $log_data  = ob_get_contents();
+    ob_end_clean();
+
+    if (@$fp = fopen('changelog.txt', 'w')) {
+         fwrite($fp, $log_data);
+         fclose($fp);
+    }
+
+    exit;
+}
+
+// If we get to here then something was wrong.
+
+echo "You must specify a date in the format YYYY-MM-DD.\n";
 
 ?>

9d5cee45521535a692215e856dadd92a7a9ce89b
HeyRatFans <matt@heyratfans.co.uk>
2006-09-10 11:59:35 +0000

    Fixed: Double encoded HTML entities in RSS feed.

--- forum/threads_rss.php
+++ forum/threads_rss.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: threads_rss.php,v 1.27 2006-07-30 16:19:27 decoyduck Exp $ */
+/* $Id: threads_rss.php,v 1.28 2006-09-10 11:59:35 decoyduck Exp $ */
 
 // Constant to define where the include files are
 define("BH_INCLUDE_PATH", "./include/");
@@ -49,6 +49,7 @@ $forum_settings = forum_get_settings();
 
 include_once(BH_INCLUDE_PATH. "db.inc.php");
 include_once(BH_INCLUDE_PATH. "format.inc.php");
+include_once(BH_INCLUDE_PATH. "html.inc.php");
 include_once(BH_INCLUDE_PATH. "logon.inc.php");
 include_once(BH_INCLUDE_PATH. "messages.inc.php");
 include_once(BH_INCLUDE_PATH. "post.inc.php");
@@ -58,15 +59,12 @@ $webtag = get_webtag($webtag_search);
 
 // Get the forum location accounting for forward slashes, multiple slashes, etc.
 
-$forum_location = preg_replace("/\\\/", "/", dirname($_SERVER['PHP_SELF']));
-$forum_location = preg_replace("/[\/]+$/", "", $forum_location);
-$forum_location = preg_replace("/^[\/]+/", "", $forum_location);
-$forum_location = preg_replace("/[\/]+/", "/", "/$forum_location/");
-$forum_location = "{$_SERVER['HTTP_HOST']}{$forum_location}";
+$forum_location = html_get_forum_uri();
 
 // Get the Forum Name and the current date.
 
 $forum_name = forum_get_setting('forum_name', false, 'A Beehive Forum');
+
 $build_data = gmdate("D, d M Y H:i:s");
 
 // Check to see if we can login a user
@@ -76,7 +74,7 @@ if (isset($_COOKIE['bh_remember_username'][0]) && isset($_COOKIE['bh_remember_pa
     $username = strtoupper($_COOKIE['bh_remember_username'][0]);
     $passhash = $_COOKIE['bh_remember_passhash'][0];
 
-    $uid = user_logon($username, $passhash, true);
+    $uid = user_logon($username, $passhash);
 
     if (isset($uid) && $uid > -1) {
 
@@ -121,8 +119,8 @@ echo "<?xml version=\"1.0\"?>\n";
 echo "<rss version=\"2.0\">\n";
 echo "<channel>\n";
 echo "<title>{$forum_name}</title>\n";
-echo "<link>http://{$forum_location}</link>\n";
-echo "<description>{$forum_name} - {$forum_location}</description>\n";
+echo "<link>{$forum_location}/</link>\n";
+echo "<description>{$forum_name} - {$forum_location}/</description>\n";
 echo "<lastBuildDate>{$build_data} UT</lastBuildDate>\n";
 echo "<generator>$forum_name / www.beehiveforum.net</generator>\n";
 
@@ -138,21 +136,19 @@ if ($threads_array = threads_get_most_recent($limit)) {
 
         $modified_date = gmdate("D, d M Y H:i:s", $thread['MODIFIED']);
 
-        $message_content = trim(message_get_content($thread['TID'], $thread['LENGTH']));
-        $parsed_message  = new MessageTextParse($message_content, false);
+        $t_content = message_get_content($thread['TID'], $thread['LENGTH']);
+        $t_content = strip_tags(trim($t_content));
 
-        $t_title = htmlentities($thread['TITLE']);
-
-        $t_content = $parsed_message->getMessage();
-        $t_content = htmlentities(strip_tags(trim($t_content)));
+        $t_content = preg_replace("/(&[^;]+;)/me", "xml_literal_to_numeric('\\1')", $t_content);
+        $t_title = preg_replace("/(&[^;]+;)/me", "xml_literal_to_numeric('\\1')", $thread['TITLE']);
 
         echo "<item>\n";
-        echo "<guid isPermaLink=\"true\">http://{$forum_location}?webtag=$webtag&amp;msg={$thread['TID']}.1</guid>\n";
+        echo "<guid isPermaLink=\"true\">{$forum_location}/?webtag=$webtag&amp;msg={$thread['TID']}.1</guid>\n";
         echo "<pubDate>{$modified_date} UT</pubDate>\n";
-        echo "<title><![CDATA[{$t_title}]]></title>\n";
-        echo "<link>http://{$forum_location}?webtag=$webtag&amp;msg={$thread['TID']}.1</link>\n";
-        echo "<description><![CDATA[{$t_content}]]></description>\n";
-        echo "<comments>http://{$forum_location}?webtag=$webtag&amp;msg={$thread['TID']}.1</comments>\n";
+        echo "<title>{$t_title}</title>\n";
+        echo "<link>{$forum_location}/?webtag=$webtag&amp;msg={$thread['TID']}.1</link>\n";
+        echo "<description>{$t_content}</description>\n";
+        echo "<comments>{$forum_location}/?webtag=$webtag&amp;msg={$thread['TID']}.1</comments>\n";
         echo "</item>\n";
     }
 }

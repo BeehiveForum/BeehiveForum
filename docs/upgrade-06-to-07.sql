a6a00dc443ed6e087d99e411d415922a264340c3
Matt Beale <matt@beehiveforum.co.uk>
2006-10-13 18:37:27 +0000

    Changed: Updated release notes for 0.7.1 Added: Images for up down controls would be best off in CVS. Duh.

---
+++ docs/upgrade-06-to-07.sql
@@ -0,0 +1,8 @@
+# --------------------------------------------------------
+#
+# This script has been supersceeded by the Beehive installer
+# which you will find located at /forum/install/install.php
+#
+# --------------------------------------------------------
+#
+# $Id: upgrade-06-to-07.sql,v 1.1 2006-10-13 18:37:27 decoyduck Exp $

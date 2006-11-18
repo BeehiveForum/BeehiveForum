99a3bb69aaf0237a35b3dcb588cd63b6735aad68
Matt Beale <matt@beehiveforum.co.uk>
2006-11-18 17:59:33 +0000

    Fixed: You could search for words in the MySQL stop word list and end up with 0 results rather than an error message. Added: Stop words popup now highlights words that were in error.

--- forum/js/search.js
+++ forum/js/search.js
@@ -24,7 +24,7 @@ function enable_search_button() {
     enable_button(top.frames['main'].frames['right'].document.forms['search_form'].go_button);
 }
 
-function display_mysql_stopwords(webtag) {
+function display_mysql_stopwords(webtag, keywords) {
 
-    window.open('search.php?webtag=' + webtag + '&show_stop_words=true', 'show_stop_words', 'width=580, height=450, scrollbars=yes, scrolling=yes');
+    window.open('search.php?webtag=' + webtag + '&show_stop_words=true&keywords=' + keywords, 'show_stop_words', 'width=580, height=450, scrollbars=yes, scrolling=yes');
 }

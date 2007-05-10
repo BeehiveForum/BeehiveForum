f71f7ee5bb4bccfbb829ba5863894e11e48e5cec
Matt Beale <matt@beehiveforum.co.uk>
2007-05-10 23:03:43 +0000

    Fixed: PM Search results to and from column data in wrong order.

--- forum/pm_messages.php
+++ forum/pm_messages.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: pm_messages.php,v 1.13 2007-05-10 22:03:17 decoyduck Exp $ */
+/* $Id: pm_messages.php,v 1.14 2007-05-10 23:03:43 decoyduck Exp $ */
 
 // Constant to define where the include files are
 define("BH_INCLUDE_PATH", "./include/");
@@ -519,15 +519,15 @@ if (isset($pm_messages_array['message_array']) && sizeof($pm_messages_array['mes
         }elseif ($current_folder == PM_SEARCH_RESULTS) {
 
             echo "                  <td align=\"left\" class=\"postbody\">{$pm_folder_name_array[$message['TYPE']]}</td>\n";
-            
-            echo "                  <td align=\"left\" class=\"postbody\">";
-            echo "            <a href=\"user_profile.php?webtag=$webtag&amp;uid={$message['TO_UID']}\" target=\"_blank\" onclick=\"return openProfile({$message['TO_UID']}, '$webtag')\">";
-            echo word_filter_add_ob_tags(format_user_name($message['TLOGON'], $message['TNICK'])) . "</a>";
-            echo "            </td>\n";
 
             echo "                  <td align=\"left\" class=\"postbody\">";
             echo "            <a href=\"user_profile.php?webtag=$webtag&amp;uid={$message['FROM_UID']}\" target=\"_blank\" onclick=\"return openProfile({$message['FROM_UID']}, '$webtag')\">";
             echo word_filter_add_ob_tags(format_user_name($message['FLOGON'], $message['FNICK'])) . "</a>";
+            echo "            </td>\n";
+
+            echo "                  <td align=\"left\" class=\"postbody\">";
+            echo "            <a href=\"user_profile.php?webtag=$webtag&amp;uid={$message['TO_UID']}\" target=\"_blank\" onclick=\"return openProfile({$message['TO_UID']}, '$webtag')\">";
+            echo word_filter_add_ob_tags(format_user_name($message['TLOGON'], $message['TNICK'])) . "</a>";
             echo "            </td>\n";
 
         }else {

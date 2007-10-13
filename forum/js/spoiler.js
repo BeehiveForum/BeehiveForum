6f223a7c637a25bc3db3c0950700037014e0e1d5
Matt Beale <matt@beehiveforum.co.uk>
2007-10-13 19:36:51 +0000

    Changed: Removed is_defined() function as it doesn't work correctly with all browsers.

--- forum/js/spoiler.js
+++ forum/js/spoiler.js
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: spoiler.js,v 1.5 2007-04-25 21:34:32 decoyduck Exp $ */
+/* $Id: spoiler.js,v 1.6 2007-10-13 19:36:51 decoyduck Exp $ */
 
 function containsDOM(container, containee)
 {
@@ -79,7 +79,7 @@ function spoilerInitialise()
 
 function spoilerAttachEvent(element)
 {
-    if (is_defined(element.tagName)) {
+    if (typeof(element.tagName) != 'undefined')) {
                
         if (IE) {
 

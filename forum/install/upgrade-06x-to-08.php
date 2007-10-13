9977c495dd848cc71a2165660e3c12733a568a14
Matt Beale <matt@beehiveforum.co.uk>
2007-10-13 21:06:44 +0000

    Changed: 0.7.2 renamed to 0.8.

--- forum/install/upgrade-06x-to-08.php
+++ forum/install/upgrade-06x-to-08.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: upgrade-06x-to-072.php,v 1.24 2007-10-12 23:28:14 decoyduck Exp $ */
+/* $Id: upgrade-06x-to-08.php,v 1.1 2007-10-13 21:06:44 decoyduck Exp $ */
 
 if (isset($_SERVER['PHP_SELF']) && basename($_SERVER['PHP_SELF']) == "upgrade-06x-to-072.php") {
 

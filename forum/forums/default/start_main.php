649ba4c5b431c4fcb53f69ec04e8837c6e31e5bb
HeyRatFans <matt@heyratfans.co.uk>
2007-11-02 22:05:03 +0000

    Changed: Removed Teh Forum Gallery script and replaced it with generic welcome page.

--- forum/forums/default/start_main.php
+++ forum/forums/default/start_main.php
@@ -1,32 +1,8 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
-<head>
-<title>Teh Forum</title>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="generator" content="Beehive Forum 0.8-CVS" />
-<meta name="keywords" content="Beehive, Teh, Tehforum, Clever, Idiot, Cretins, Nerds, Geeks, Nice" />
-<meta name="description" content="Home of the Beehive, a collection of whimsical fools and clever people." />
-<meta name="robots" content="noindex,follow" />
-<link rel="stylesheet" href="styles/Default/style.css?20071023002618" type="text/css" media="screen" />
-<link rel="stylesheet" href="emoticons/default/style.css?20040908165530" type="text/css" />
-<link rel="search" type="application/opensearchdescription+xml" href="http://onthefloor.f2s.com/forum/search.php?webtag=DEFAULT&amp;opensearch" title="Teh Forum" />
-<script language="Javascript" type="text/javascript">
-<!--
-
-var bh_frame_main = 'bh_frame_cbaeceabccfad'
-var bh_frame_ftop = 'bh_frame_bcfceedbceef'
-var bh_frame_fnav = 'bh_frame_dcdfcefafeff'
-var bh_frame_left = 'bh_frame_feaffbeaddcea'
-var bh_frame_right = 'bh_frame_abacdbafcdc'
-var bh_frame_pm_folders = 'bh_frame_ecaaceafcc'
-var bh_frame_pm_messages = 'bh_frame_dcfcafadceb'
-
-//-->
-</script>
-<script language="Javascript" type="text/javascript" src="./js/general.js?20071027205721"></script>
-</head>
-
-<body>
-<h1>Woo</h1></body>
-</html>
+<h1>Welcome to your new Beehive Forum!</h1>
+<p><a href="http://www.beehiveforum.net/" target="_blank">Home</a> | <a href="http://beehiveforum.net/faq" target="_blank">FAQ</a> | <a href="http://sourceforge.net/docman/?group_id=50772" target="_blank">Docs</a> | <a href="http://sourceforge.net/project/showfiles.php?group_id=50772" target="_blank">Download</a> | <a href="http://www.beehiveforum.net/forums.php" target="_blank">Live Forums</a></p>
+<table cellpadding="0" cellspacing="0" width="96%" class="success_msg">
+  <tr>
+    <td valign="top" width="25" class="warning_msg_icon"><img src="images/warning.png" width="15" height="15" alt="Warning" title="Warning" /></td>
+    <td valign="top" class="warning_msg_icon">You can modify this start page from the admin interface.</td>
+  </tr>
+</table>

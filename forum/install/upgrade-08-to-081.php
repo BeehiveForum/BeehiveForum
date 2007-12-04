34c1e2e173ca757e7cd3e9205b1c0193a924230c
HeyRatFans <matt@heyratfans.co.uk>
2007-12-04 20:27:25 +0000

    Updated: Replaced 0.8 upgrade scripts with new 0.8.1 scritps. Added: Upgrade script to go from 0.8 to 0.8.1

---
+++ forum/install/upgrade-08-to-081.php
@@ -0,0 +1,83 @@
+<?php
+
+/*======================================================================
+Copyright Project Beehive Forum 2002
+
+This file is part of Beehive Forum.
+
+Beehive Forum is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+Beehive Forum is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Beehive; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+USA
+======================================================================*/
+
+/* $Id: upgrade-08-to-081.php,v 1.1 2007-12-04 20:27:25 decoyduck Exp $ */
+
+@set_time_limit(0);
+
+$forum_webtag_array = array();
+
+// This script upgrades all forums it finds regardless of the
+// WEBTAG entered in the install form. This is imperative that
+// this happens because otherwise if you later try to upgrade
+// a second forum you will run into problems
+
+$sql = "SHOW TABLES LIKE 'FORUMS'";
+
+if (!$result = @db_query($sql, $db_install)) {
+
+    $error_html.= "<h2>Could not locate any previous Beehive Forum installations!</h2>\n";
+    $valid = false;
+    return;
+}
+
+if (db_num_rows($result) > 0) {
+
+    $sql = "SELECT FID, WEBTAG FROM FORUMS";
+
+    if ($result = @db_query($sql, $db_install)) {
+
+        while ($forum_data = @db_fetch_array($result)) {
+
+            $forum_webtag_array[$forum_data['FID']] = $forum_data['WEBTAG'];
+        }
+
+    }else {
+
+        $error_html.= "<h2>Could not locate any previous Beehive Forum installations!</h2>\n";
+        $valid = false;
+        return;
+    }
+}
+
+// We got this far then everything is okay for all forums.
+// Start by creating and updating the per-forum tables.
+
+foreach($forum_webtag_array as $forum_fid => $forum_webtag) {
+
+    $sql = "INSERT INTO {$forum_webtag}_THREAD (TID, FID, BY_UID, TITLE, LENGTH, ";
+    $sql.= "POLL_FLAG, CREATED, MODIFIED, CLOSED, STICKY, STICKY_UNTIL, ADMIN_LOCK) ";
+    $sql.= "SELECT THREAD.TID, THREAD.FID, THREAD.BY_UID, THREAD.TITLE, THREAD.LENGTH, ";
+    $sql.= "THREAD.POLL_FLAG, THREAD.CREATED, MAX(POST.CREATED), THREAD.CLOSED, THREAD.STICKY, ";
+    $sql.= "THREAD.STICKY_UNTIL, THREAD.ADMIN_LOCK FROM {$forum_webtag}_THREAD THREAD ";
+    $sql.= "LEFT JOIN {$forum_webtag}_POST POST ON (POST.TID = THREAD.TID) GROUP BY THREAD.TID ";
+    $sql.= "ON DUPLICATE KEY UPDATE MODIFIED = VALUES(MODIFIED)";
+
+    if (!$result = @db_query($sql, $db_install)) {
+
+        $valid = false;
+        return;
+    }
+}
+
+?>

630a214a82afcba47e41e0f08144be5cce755030
Matt Beale <matt@beehiveforum.co.uk>
2008-01-28 21:40:27 +0000

    Fixed: Supressed errors using db_unbuffered_query() didn't show the MySQL error string when developer mode was switched on.

--- forum/include/db/db_mysql.inc.php
+++ forum/include/db/db_mysql.inc.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: db_mysql.inc.php,v 1.45 2008-01-28 21:38:18 decoyduck Exp $ */
+/* $Id: db_mysql.inc.php,v 1.46 2008-01-28 21:40:27 decoyduck Exp $ */
 
 function db_get_connection_vars(&$db_server, &$db_username, &$db_password, &$db_database)
 {
@@ -101,7 +101,7 @@ function db_unbuffered_query($sql, $connection_id, $trigger_error = true)
         if ($trigger_error === true) db_trigger_error($sql, $connection_id);
     }
 
-    return db_query($sql, $connection_id);
+    return db_query($sql, $connection_id, $trigger_error);
 }
 
 function db_data_seek($result, $offset)

f7db6c59d692820dbe9cac522164e23937cac3ee
Matt Beale <matt@beehiveforum.co.uk>
2008-03-22 16:01:13 +0000

    *** empty log message ***

--- forum/js/stats.js
+++ forum/js/stats.js
@@ -1,3 +1,26 @@
+/*======================================================================
+Copyright Project BeehiveForum 2002
+
+This file is part of BeehiveForum.
+
+BeehiveForum is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BeehiveForum is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Beehive; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+USA
+======================================================================*/
+
+/* $Id: stats.js,v 1.2 2008-03-22 16:01:13 decoyduck Exp $ */
+
 var stats_timeout;
 
 var stats_data = new xml_http_request();

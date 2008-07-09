22fbdf0325c906e7baae9f0ef7425592a5184e5c
HeyRatFans <matt@heyratfans.co.uk>
2008-07-09 18:01:35 +0000

    Changed: Folder warning message in links.php now uses html_display_warning_msg() function. Fixed: Drop Down List appearing as a multi-line text field in Profile.

--- forum/links.php
+++ forum/links.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: links.php,v 1.110 2008-03-24 23:32:15 decoyduck Exp $ */
+/* $Id: links.php,v 1.111 2008-07-09 18:01:35 decoyduck Exp $ */
 
 // Constant to define where the include files are
 define("BH_INCLUDE_PATH", "include/");
@@ -315,7 +315,11 @@ if ($viewmode == LINKS_VIEW_HIERARCHICAL) {
             echo "              <td>&nbsp;</td>\n";
             echo "            </tr>\n";
             echo "            <tr>\n";
-            echo "              <td>{$lang['linksdelexp']}</td>\n";
+            echo "              <td>\n";
+
+            html_display_warning_msg($lang['linksdelexp'], '100%', 'left');
+
+            echo "              </td>\n";
             echo "            </tr>\n";
             echo "            <tr>\n";
             echo "              <td>&nbsp;</td>\n";

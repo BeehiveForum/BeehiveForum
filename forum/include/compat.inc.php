4364a0c805b76154c5c9a3a639961a2df2822f63
Matt Beale <matt@beehiveforum.co.uk>
2008-10-26 18:15:03 +0000

    Changed: Added more functions to compat.inc.php in preperation for multi-byte support in Beehive.

--- forum/include/compat.inc.php
+++ forum/include/compat.inc.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: compat.inc.php,v 1.3 2008-10-26 16:46:27 decoyduck Exp $ */
+/* $Id: compat.inc.php,v 1.4 2008-10-26 18:15:03 decoyduck Exp $ */
 
 /**
 * compat.inc.php - Compatibility functions
@@ -36,6 +36,27 @@ if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
     header("Content-Location: ../index.php");
     header("Location: ../index.php");
     exit;
+}
+
+/**
+* Wrapper function for mb_send_mail.
+*
+* Send mail.
+*
+* @return boolean
+* @param string $to - Receiver, or receivers of the mail
+* @param string $subject - Subject of the email to be sent.
+* @param string $message - Message to be sent.
+* @param string $additional_headers - String to be inserted at the end of the email header.
+* @param string $additional_parameters - Additional parameter to send to the program configured to use when sending mail.
+*/
+
+if (!function_exists('mb_send_mail')) {
+
+    function mb_send_mail($to, $subject, $message, $additional_headers = '', $additional_parameters = '')
+    {
+        return mail($to, $subject, $message, $additional_headers, $additional_parameters);
+    }
 }
 
 /**
@@ -199,6 +220,82 @@ if (!function_exists('mb_split')) {
     function mb_split($pattern, $str, $limit = null)
     {
         return is_null($limit) ? split($pattern, $str) : split($pattern, $str, $limit);
+    }
+}
+
+/**
+* Wrapper function for mb_ereg.
+*
+* Regular expression match
+*
+* @return array
+* @param string $pattern - Case sensitive regular expression.
+* @param string $str - The input string
+* @param integer $regs - Optional - matches found for parenthesized substrings of pattern (by-ref)
+*/
+
+if (!function_exists('mb_ereg')) {
+
+    function mb_ereg($pattern, $str, &$regs = array())
+    {
+        return ereg($pattern, $str, $regs);
+    }
+}
+
+/**
+* Wrapper function for mb_eregi.
+*
+* Regular expression match
+*
+* @return array
+* @param string $pattern - Case insensitive regular expression.
+* @param string $str - The input string
+* @param integer $regs - Optional - matches found for parenthesized substrings of pattern (by-ref)
+*/
+
+if (!function_exists('mb_eregi')) {
+
+    function mb_eregi($pattern, $str, &$regs = array())
+    {
+        return eregi($pattern, $str, $regs);
+    }
+}
+
+/**
+* Wrapper function for mb_ereg_replace.
+*
+* Replace regular expression case sensitive
+*
+* @return array
+* @param string $pattern - A POSIX extended regular expression.
+* @param string $replacement - Replacements for parenthesized substring matches in pattern
+* @param integer $string - The input string.
+*/
+
+if (!function_exists('mb_ereg_replace')) {
+
+    function mb_ereg_replace($pattern, $replacement, $string)
+    {
+        return ereg_replace($pattern, $replacement, $string);
+    }
+}
+
+/**
+* Wrapper function for mb_eregi_replace.
+*
+* Replace regular expression case insensitive
+*
+* @return array
+* @param string $pattern - A POSIX extended regular expression.
+* @param string $replacement - Replacements for parenthesized substring matches in pattern
+* @param integer $string - The input string.
+*/
+
+if (!function_exists('mb_eregi_replace')) {
+
+    function mb_eregi_replace($pattern, $replacement, $string)
+    {
+        return eregi_replace($pattern, $replacement, $string);
     }
 }
 

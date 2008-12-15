ddbd312f69f61a57a649921d688c48c88bfc1039
Matt Beale <matt@beehiveforum.co.uk>
2008-12-15 07:39:48 +0000

    Fixed: Elevated PHP minimum requirements to PHP 5.2.1.

--- bh_check_php_version.php
+++ bh_check_php_version.php
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 USA
 ======================================================================*/
 
-/* $Id: bh_check_php_version.php,v 1.9 2008-06-05 19:59:15 decoyduck Exp $ */
+/* $Id: bh_check_php_version.php,v 1.10 2008-12-15 07:39:48 decoyduck Exp $ */
 
 // Requires PHP PEAR to be installed and PHP_CompatInfo Class.
 // See: http://www.laurent-laville.org/index.php?module=pear&desc=pci
@@ -32,11 +32,7 @@ require_once 'PHP/CompatInfo.php';
 
 $options = array('debug'            => false,  // Debug mode
                  'recurse_dir'      => true,  // Recurse all directories below 'forum'
-                 'ignore_constants' => array('E_STRICT'), // Ignored PHP 5 constant used by errorhandler.inc.php
-                 'ignore_functions' => array('sys_get_temp_dir', 'file_put_contents'), // Ignore these functions.
-                 'ignore_dirs'      => array('forum\geshi', 'forum\tiny_mce'),     // Ignore Geshi and TinyMCE
-                 'ignore_files'     => array('forum\include\db\db_mysql.inc.php',    // Ignore our DB connection scripts.
-                                             'forum\include\db\db_mysqli.inc.php')); // We know these work on PHP4.
+                 'ignore_dirs'      => array('forum\geshi', 'forum\tiny_mce'));     // Ignore Geshi and TinyMCE
 
 // Tell the user what we're doing.
 

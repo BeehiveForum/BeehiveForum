6729ff699838f20ca1a859f7c15ce229f8ada148
Matt Beale <matt@beehiveforum.co.uk>
2009-07-11 19:16:48 +0000

    Changed: Relaxed checking on $cookie_path and $cookie_domain in bh_setcookie. Changed: Removed microtime_float function as we don't use it. Added: bh_check_function_names.php which checks the function names all start with their filename. Prep work for future autoload of includes. Added: bh_x7chat_auth.php for automatic logon using BH's authentication to X7Chat. Added: Alternative top.php which can be used in place of top.html.

---
+++ bh_check_function_names.php
@@ -0,0 +1,76 @@
+<?php
+
+/*======================================================================
+Copyright Project BeehiveForum 2002
+
+This file is part of BeehiveForum.
+
+BeehiveForum is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BeehiveForum is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Beehive; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+USA
+======================================================================*/
+
+/* $Id: bh_check_function_names.php,v 1.1 2009-07-11 19:16:48 decoyduck Exp $ */
+
+// Callback function to escape array of strings.
+
+function preg_quote_callback($str)
+{
+    return preg_quote($str, "/");
+}
+
+// Array to hold source files
+
+$source_files_array = array();
+
+// Path to source files.
+
+$source_files_dir_array = array('forum\include');
+
+// Ignore these files
+
+$ignore_files_array = array('dictionary.inc.ph', 'text_captcha.inc.php');
+
+echo "Getting list of functions...\n";
+
+foreach ($source_files_dir_array as $include_file_dir) {
+
+    if (($dir = opendir($include_file_dir))) {
+
+        while (($file = readdir($dir)) !== false) {
+
+            $path_info_array = pathinfo("$include_file_dir\\$file");
+            
+            if (isset($path_info_array['extension']) && $path_info_array['extension'] == 'php' && !in_array($path_info_array['basename'], $ignore_files_array)) {
+            
+                $function_prefix = str_replace('.inc.php', '', $file);
+            
+                $source_file_contents = file_get_contents("$include_file_dir\\$file");
+                
+                if (preg_match_all("/function ([a-z_-]+)\s?\(([^\)]*)\)/", $source_file_contents, $function_matches_array, PREG_SET_ORDER) > 0) {
+                    
+                    foreach ($function_matches_array as $function_match) {
+                    
+                        if (substr($function_match[1], 0, strlen($function_prefix)) != $function_prefix) {
+                        
+                            echo $file, ": ", $function_match[0], "\n";
+                        }
+                    }
+                }            
+            }
+        }
+    }
+}
+
+?>
---
+++ bh_x7chat_auth.php
@@ -0,0 +1,128 @@
+<?php
+
+/*======================================================================
+Copyright Project Beehive Forum 2002
+
+This file is part of Beehive Forum.
+
+Beehive Forum is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+Beehive Forum is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Beehive; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+USA
+======================================================================*/
+
+/* $Id: bh_x7chat_auth.php,v 1.1 2009-07-11 19:16:48 decoyduck Exp $ */
+
+// Set the default timezone
+date_default_timezone_set('UTC');
+
+// Constant to define where the include files are
+define("BH_INCLUDE_PATH", "/beehiveforum/home/beehiveforum/forum/include/");
+
+// Server checking functions
+include_once(BH_INCLUDE_PATH. "server.inc.php");
+
+// Installation checking functions
+include_once(BH_INCLUDE_PATH. "install.inc.php");
+
+// Check that Beehive is installed correctly
+check_install();
+
+// Multiple forum support
+include_once(BH_INCLUDE_PATH. "forum.inc.php");
+
+// Fetch Forum Settings
+
+$forum_settings = forum_get_settings();
+
+// Fetch Global Forum Settings
+
+$forum_global_settings = forum_get_global_settings();
+
+include_once(BH_INCLUDE_PATH. "db.inc.php");
+include_once(BH_INCLUDE_PATH. "session.inc.php");
+
+// X7Chat Username Cookie
+
+$auth_ucookie = "X7C2U";
+
+// X7Chat Username Cookie
+
+$auth_pcookie = "X7C2P";
+
+// Registration link
+
+$auth_register_link = "../register.php";
+
+// Disable Guest access to X7Chat
+
+$auth_disable_guest = true;
+
+// See if we can try and logon automatically
+
+logon_perform_auto();
+
+// Check the session is active
+
+if (($user_sess = bh_session_check())) {
+
+    if ($user_data = user_get(bh_session_get_value('UID'))) {
+        
+        $_COOKIE[$auth_ucookie] = db_escape_string($user_data['LOGON']);
+        $_COOKIE[$auth_pcookie] = db_escape_string($user_data['PASSWD']);
+        
+        $query = $db->DoQuery("SELECT * FROM {$prefix}users WHERE username='$_COOKIE[$auth_ucookie]'");
+        
+        $row = $db->Do_Fetch_Row($query);
+        
+        if ($row[0] == '') {
+        
+            $time = time();
+            
+            $ip = db_escape_string($_SERVER['REMOTE_ADDR']);
+            
+            $activated = bh_session_user_approved() ? '1' : '0';
+            
+            $email = db_escape_string($user_data['EMAIL']);
+            
+            $db->DoQuery("INSERT INTO {$prefix}users (username,password,email,user_group,time,settings,hideemail,ip,activated) VALUES('$_COOKIE[$auth_ucookie]','$_COOKIE[$auth_pcookie]','{$email}', '{$x7c->settings['usergroup_default']}','$time','{$g_default_settings}','1','$ip','$activated')");
+        }        
+    }
+}
+
+// Authentication Encryption.
+
+function auth_encrypt($data)
+{
+    return md5($data);
+}
+
+// Get password hash
+
+function auth_getpass($auth_ucookie)
+{
+    if ($user_data = user_get_by_logon($_COOKIE[$auth_ucookie])) {
+        return $user_data['PASSWD'];
+    }
+}
+
+// Change password
+
+function change_pass($user, $newpass)
+{
+    if ($user_data = user_get_by_logon($user)) {
+        user_change_password($user_data['UID'], $newpass, $user_data['PASSWD']);
+    }
+}
+
+?>

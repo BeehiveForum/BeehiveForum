6e93bcfdaaf6d4bf4bbba74658a878677d9393e7
HeyRatFans <matt@heyratfans.co.uk>
2009-10-28 20:07:53 +0000

    Added: Edd's emoticon pack now officially part of Beehive.

---
+++ forum/emoticons/edd/definitions.php
@@ -0,0 +1,313 @@
+<?php
+
+/*======================================================================
+Copyright Project BeehiveForum 2002
+
+This file is part of BeehiveForum.
+
+BeehiveForum is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BeehiveForum is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Beehive; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+USA
+======================================================================*/
+
+// Define the emoticon patterns to be matched, and their associated names
+
+// Default emoticons
+$emoticon[':-)'] = "smile";
+$emoticon[':)'] = "smile";
+$emoticon[';-)'] = "wink";
+$emoticon[';)'] = "wink";
+$emoticon[':-('] = "sad";
+$emoticon[':('] = "sad";
+$emoticon[':\'-('] = "cry";
+$emoticon[':\'('] = "cry";
+$emoticon[':-P'] = "tongue";
+$emoticon[':P'] = "tongue";
+$emoticon[':-D'] = "grin";
+$emoticon[':D'] = "grin";
+$emoticon[':-S'] = "confused";
+$emoticon[':S'] = "confused";
+$emoticon[':-|'] = "plain";
+$emoticon[':|'] = "plain";
+$emoticon[':-/'] = "unsure";
+$emoticon[':/'] = "unsure";
+$emoticon[':-O'] = "shock";
+$emoticon[':O'] = "shock";
+$emoticon[':-@'] = "angry";
+$emoticon[':@'] = "angry";
+$emoticon[':-$'] = "embarrassed";
+$emoticon[':$'] = "embarrassed";
+$emoticon['(angel)'] = "angel";
+$emoticon['(devil)'] = "devil";
+$emoticon['B-)'] = "shades";
+$emoticon['B)'] = "shades";
+$emoticon['(shades)'] = "shades";
+
+// Peter Boughton's emoticons
+$emoticon[':>'] = "cheeky";
+$emoticon[':->'] = "cheeky";
+$emoticon['}:>'] = "evil_cheeky";
+$emoticon['}:->'] = "evil_cheeky";
+$emoticon[':O)'] = "smile_big_nose";
+$emoticon['>.<'] = "cringe";
+$emoticon['^.^'] = "happy";
+$emoticon['<_<'] = "sidelook";
+
+// Trillian emoticons by Jay Graham
+$emoticon[':-<'] = "anglesad";
+$emoticon[':<'] = "anglesad";
+$emoticon['T_T'] = "acidcry";
+$emoticon['=:)'] = "alien";
+$emoticon[':-(('] = "crying";
+$emoticon[':(('] = "crying";
+$emoticon['%-)'] = "drunk";
+$emoticon['%)'] = "drunk";
+$emoticon[':\'-D'] = "happycry";
+$emoticon[':\'D'] = "happycry";
+$emoticon[':-&'] = "horror";
+$emoticon[':&'] = "horror";
+$emoticon[':-{)'] = "moustache";
+$emoticon[':{)'] = "moustache";
+$emoticon[':-O~~~'] = "partyblower";
+$emoticon[':-Y'] = "pipe";
+$emoticon[':Y'] = "pipe";
+$emoticon[':-B'] = "rabbit";
+$emoticon[':B'] = "rabbit";
+$emoticon['I-)'] = "sleepyhead";
+$emoticon['I)'] = "sleepyhead";
+$emoticon[':~)'] = "bigtear1";
+$emoticon[':\')'] = "bigtear";
+$emoticon[':\'C'] = "Ccry";
+$emoticon[':\'-C'] = "Ccry";
+$emoticon[':-['] = "crookedsad";
+$emoticon[':['] = "crookedsad";
+$emoticon[':-]'] = "crookedsmile";
+$emoticon[':]'] = "crookedsmile";
+$emoticon[':C'] = "Csad";
+$emoticon[':-C'] = "Csad";
+$emoticon[':J'] = "curlyhappy";
+$emoticon[':-J'] = "curlyhappy";
+$emoticon[':L'] = "curlysad";
+$emoticon[':-L'] = "curlysad";
+$emoticon['$)'] = "moneyeyes";
+$emoticon['$-)'] = "moneyeyes";
+$emoticon[':?'] = "questioned";
+$emoticon[':-?'] = "questioned";
+$emoticon[':X'] = "stitched";
+$emoticon[':-X'] = "stitched";
+$emoticon[':><'] = "zippedmouth";
+$emoticon[':-><'] = "zippedmouth";
+$emoticon[':\'S'] = "tear_confused";
+$emoticon[':T'] = "tsmile";
+$emoticon[':-T'] = "tsmile";
+$emoticon['8-|'] = "geekupset";
+$emoticon['8|'] = "geekupset";
+$emoticon['8-O'] = "eek";
+$emoticon['8)'] = "shadeshead";
+$emoticon['8-)'] = "shadeshead";
+$emoticon['8-P'] = "shadestongue";
+$emoticon['X-O'] = "surprised_dead";
+$emoticon['X-P'] = "tongue_dead";
+$emoticon['X-S'] = "confused_dead";
+$emoticon['>-)'] = "vince2";
+$emoticon['>-|'] = "vince_upset";
+$emoticon['>-D'] = "vincehappy";
+$emoticon['>-O'] = "vince_surprise";
+$emoticon[':XO'] = "warningface";
+$emoticon[';P'] = "wink_tongue";
+$emoticon[';-P'] = "wink_tongue";
+
+$emoticon['LMAO'] = "LMAO";
+$emoticon['LOL'] = "LOL";
+$emoticon['(beer)'] = "beer";
+$emoticon['(bounce)'] = "bounce";
+$emoticon['(boy)'] = "boy";
+$emoticon['(heartbroke)'] = "brokenheart";
+$emoticon['(bat)'] = "bat";
+$emoticon['(bug)'] = "bug";
+$emoticon['(bunny)'] = "bunny";
+$emoticon['(cake)'] = "cake";
+$emoticon['(camera)'] = "camera";
+$emoticon['(car)'] = "car";
+$emoticon['(cat)'] = "cat";
+$emoticon['(cheer)'] = "cheering";
+$emoticon['(cig)'] = "cig";
+$emoticon['(clock)'] = "clock";
+$emoticon['(cow)'] = "cow";
+$emoticon['(cowboy)'] = "cowboy";
+$emoticon['(dance)'] = "dance";
+$emoticon['(dog)'] = "dog";
+$emoticon['(dove)'] = "dove";
+$emoticon['(drink)'] = "drink";
+$emoticon['(drool)'] = "drool";
+$emoticon['(drums)'] = "drums";
+$emoticon['(duck)'] = "duck";
+$emoticon['(duh)'] = "duh";
+$emoticon['(envelope)'] = "envelope";
+$emoticon['(fail)'] = "fail";
+$emoticon['(fish)'] = "fish";
+$emoticon['(flower)'] = "flower";
+$emoticon['(football)'] = "football";
+$emoticon['(frog)'] = "frog";
+$emoticon['(rar)'] = "giantcool";
+$emoticon['(gift)'] = "gift";
+$emoticon['(girl)'] = "girl";
+$emoticon['(globe)'] = "globe";
+$emoticon['(heart)'] = "heartbeat";
+$emoticon['(kiss)'] = "kiss";
+$emoticon['(hugleft)'] = "lefthug";
+$emoticon['(lightbulb)'] = "lightbulb";
+$emoticon['(monkey)'] = "monkey";
+$emoticon['(moon)'] = "moon";
+$emoticon['(movie)'] = "movie";
+$emoticon['(music)'] = "musicnote";
+$emoticon['(nod)'] = "nod";
+$emoticon['(nosmoke)'] = "nosmoke";
+$emoticon['(octopus)'] = "octopus";
+$emoticon['(otter)'] = "otter";
+$emoticon['(phone)'] = "phone";
+$emoticon['(pig)'] = "pig";
+$emoticon['(plane)'] = "plane";
+$emoticon['(poke)'] = "poke";
+$emoticon['(rainbow)'] = "rainbow";
+$emoticon['(hugright)'] = "righthug";
+$emoticon['(santa)'] = "santa";
+$emoticon['(wrong)'] = "shakehead";
+$emoticon['(sheep)'] = "sheep";
+$emoticon['(snail)'] = "snail";
+$emoticon['(squirrel)'] = "squirrel";
+$emoticon['(star)'] = "star";
+$emoticon['(sun)'] = "sun";
+$emoticon['(talk)'] = "talk";
+$emoticon['(tea)'] = "tea";
+$emoticon['(no)'] = "thumbsdown";
+$emoticon['(yes)'] = "thumbsup";
+$emoticon['(toaster)'] = "toaster";
+$emoticon['(turtle)'] = "turtle";
+$emoticon['(wilt)'] = "wilt";
+$emoticon['(witch)'] = "witch";
+$emoticon['(woot)'] = "woot";
+$emoticon['(worship)'] = "worship";
+$emoticon['(angelsmile)'] = "angel_smile";
+$emoticon['(apple)'] = "apple";
+$emoticon['(appled)'] = "appledee";
+$emoticon['(argh)'] = "argh";
+$emoticon['(arseface)'] = "arseface";
+$emoticon['(attention)'] = "attention";
+$emoticon['(aww)'] = "aww";
+$emoticon['(balloons)'] = "balloons";
+$emoticon['(beep)'] = "beep";
+$emoticon['(eattears)'] = "bigtear2";
+$emoticon['(blah)'] = "blahblah";
+$emoticon['(book)'] = "book";
+$emoticon['(brb)'] = "brb";
+$emoticon['(work)'] = "briefcase";
+$emoticon['(can)'] = "can";
+$emoticon['(candycane)'] = "candycane";
+$emoticon['(cd)'] = "cd";
+$emoticon['(cent)'] = "cent";
+$emoticon['(cheese)'] = "cheese";
+$emoticon['(churchill)'] = "churchill";
+$emoticon['(clown)'] = "clown";
+$emoticon['(computer)'] = "computer";
+$emoticon['(corn)'] = "corn";
+$emoticon['(chopsticks)'] = "cupwithsticks";
+$emoticon['(devilcheeky)'] = "devil_tongue";
+$emoticon['(dice)'] = "dice";
+$emoticon['(dinner)'] = "dinner";
+$emoticon['(dollar)'] = "dollar";
+$emoticon['(eep)'] = "eep";
+$emoticon['(erm)'] = "erm";
+$emoticon['(evilchild)'] = "evilchild";
+$emoticon['(eye)'] = "eye";
+$emoticon['(fergie)'] = "fergie";
+$emoticon['(finger)'] = "finger";
+$emoticon['(communist)'] = "flag";
+$emoticon['(frown)'] = "frown";
+$emoticon['(gas)'] = "gas";
+$emoticon['(giggle)'] = "giggle";
+$emoticon['(glass)'] = "water";
+$emoticon['(grandma)'] = "grandma";
+$emoticon['(hand)'] = "hand";
+$emoticon['(handcuffs)'] = "handcuffs";
+$emoticon['(hatwtf)'] = "hatwtf";
+$emoticon['(headphones)'] = "headphones";
+$emoticon['(highfive)'] = "(highfive)";
+$emoticon['(hmm)'] = "hmm";
+$emoticon['(home)'] = "home";
+$emoticon['(hug)'] = "hug";
+$emoticon['(icecream)'] = "icecream";
+$emoticon['(ill)'] = "ill";
+$emoticon['(info)'] = "information";
+$emoticon['(inlove)'] = "inlove";
+$emoticon['(island)'] = "island";
+$emoticon['(jestercheeky)'] = "jester_tongue";
+$emoticon['(lightning)'] = "lightning";
+$emoticon['(LOLdead)'] = "LOL_dead";
+$emoticon['(manthorp)'] = "manthorp";
+$emoticon['(movie)'] = "movie";
+$emoticon['(msn)'] = "msn";
+$emoticon['(musiccd)'] = "music_cd";
+$emoticon['(stop)'] = "no";
+$emoticon['(ok)'] = "ok";
+$emoticon['(oof)'] = "oof";
+$emoticon['(oop)'] = "oop";
+$emoticon['(openmouth)'] = "openmouth";
+$emoticon['(padlock)'] = "padlock";
+$emoticon['(page)'] = "page";
+$emoticon['(pencil)'] = "pencil";
+$emoticon['(imarealboy)'] = "pinochio";
+$emoticon['(pizza)'] = "pizza";
+$emoticon['(pray)'] = "pray";
+$emoticon['(ps2)'] = "ps2";
+$emoticon['(puke)'] = "puke";
+$emoticon['(pumpkin)'] = "pumpkin";
+$emoticon['(rain)'] = "rain";
+$emoticon['(rave)'] = "rave";
+$emoticon['(search)'] = "search";
+$emoticon['(skull)'] = "skull";
+$emoticon['(mwah)'] = "smack";
+$emoticon['(sneeze)'] = "sneeze";
+$emoticon['(soccer)'] = "soccer";
+$emoticon['(steak)'] = "steak";
+$emoticon['(halt)'] = "stop";
+$emoticon['(sushi)'] = "sushi";
+$emoticon['(taboo)'] = "taboo";
+$emoticon['(turkey)'] = "turkey";
+$emoticon['(television)'] = "tv2";
+$emoticon['(tv)'] = "tv";
+$emoticon['(umbrella)'] = "umbrella";
+$emoticon['(victory)'] = "victory";
+$emoticon['(webcam)'] = "webcam";
+$emoticon['(??)'] = "what";
+$emoticon['(will)'] = "will";
+$emoticon['(wine)'] = "wine";
+$emoticon['(wizard)'] = "wizard";
+$emoticon['(xbox)'] = "xbox";
+$emoticon['(yin)'] = "yin";
+$emoticon['(yum)'] = "yum";
+$emoticon['(zooted)'] = "zooted";
+$emoticon['(hippo)'] = "hippo";
+
+// Oldskool MSN emoticons (Type R) by Edd
+$emoticon['>:('] = "angry";
+$emoticon['>:-('] = "angry";
+$emoticon['X)'] = "smile_dead";
+$emoticon['X-)'] = "smile_dead";
+$emoticon['X-D'] = "grin_dead";
+
+$emoticon['(jesus)'] = "jesus";
+$emoticon['(goth)'] = "goth";
+
+?>

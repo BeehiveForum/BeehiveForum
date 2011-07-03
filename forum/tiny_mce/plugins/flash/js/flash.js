c5923babd085f4d18ec559040fbba5192f36e55c
Matt Beale <matt@beehiveforum.co.uk>
2011-07-03 17:35:26 +0000

    Fixed: Broken HTML in TinyMCE editor after submitting post for preview with Flash "img" Changed: Styled up Flash element in TinyMCE so it is easier to find and manipulate.

--- forum/tiny_mce/plugins/flash/js/flash.js
+++ forum/tiny_mce/plugins/flash/js/flash.js
@@ -6,20 +6,39 @@ var FlashDialog = {
 
         var f = document.forms[0], n = tinyMCEPopup.editor.selection.getNode();
 
-        f.flashURL.value = n.attributes['src'].value;
-        f.flashWidth.value = n.attributes['width'].value;
-        f.flashHeight.value = n.attributes['height'].value;
+        try {
+
+            var properties = n.attributes['alt'].value.split(';wmode=');
+
+            f.flashURL.value = decodeURIComponent(properties[0]) || '';
+
+            f.flashWMode.value = decodeURIComponent(properties[1]) || 'opaque';
+
+            f.flashWidth.value = n.attributes['width'].value;
+
+            f.flashHeight.value = n.attributes['height'].value;
+
+        } catch (e) {
+
+        }
     },
 
     insert: function () {
-        // Insert the contents from the input into the document
+
         var url = document.forms[0].flashURL.value;
-        if (url === null) { tinyMCEPopup.close(); return; }
 
-        var width = document.forms[0].flashWidth.value;
-        var height = document.forms[0].flashHeight.value;
+        if (url === null) {
 
-        tinyMCEPopup.editor.execCommand('mceInsertContent', false, '<img src="tiny_mce/plugins/flash/img/blank.gif" class="mceItem flash" alt="' + url + '" width="' + width + '" height="' + height + '" />');
+            tinyMCEPopup.close();
+            return;
+        }
+
+        var width = parseInt(document.forms[0].flashWidth.value);
+        var height = parseInt(document.forms[0].flashHeight.value);
+        var wmode = document.forms[0].flashWMode.value;
+
+        tinyMCEPopup.editor.execCommand('mceInsertContent', false, '<img src="tiny_mce/plugins/flash/img/blank.gif" class="mceItem flash" alt="' + encodeURIComponent(url) + ';wmode=' + encodeURIComponent(wmode) + '" width="' + width + '" height="' + height + '" />');
+
         tinyMCEPopup.close();
     }
 };

8d9adf300f1239df86fcd7e3485d21e42bfec63e
HeyRatFans <matt@heyratfans.co.uk>
2011-07-03 14:58:41 +0000

    Added: New youtube and flash tags to HTML editor. Added: Youtube and Flash plugins for TinyMCE.

---
+++ forum/tiny_mce/plugins/flash/langs/en.js
@@ -0,0 +1,3 @@
+tinyMCE.addI18n('en.flash',{
+	desc : 'Insert Flash'
+});
---
+++ forum/tiny_mce/plugins/flash/langs/en_dlg.js
@@ -0,0 +1,8 @@
+tinyMCE.addI18n('en.flash_dlg',{
+    title: 'Insert/edit Flash',
+    url_field: 'Flash URL:',
+    size: 'Size:',
+    window_mode: 'Window Mode:',
+    opaque: 'Opaque',
+    transparent: 'Transparent',
+});

4ade22bce610e546a7a53e4d6cdf806db80a7dd1
HeyRatFans <matt@heyratfans.co.uk>
2011-07-08 17:47:55 +0000

    Changed: Normalised the styles so they're all the same sans-colours Fixed: Added missing dependency of html.inc.php in browser.inc.php

--- forum/include/browser.inc.php
+++ forum/include/browser.inc.php
@@ -32,6 +32,7 @@ if (basename($_SERVER['SCRIPT_NAME']) == basename(__FILE__)) {
 }
 
 include_once(BH_INCLUDE_PATH. "constants.inc.php");
+include_once(BH_INCLUDE_PATH. "html.inc.php");
 
 /**
 * browser_check

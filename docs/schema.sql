44c0c607547e2d23aee4cc53d64ec35c15f0aa72
HeyRatFans <matt@heyratfans.co.uk>
2012-04-06 17:54:12 +0100

    Changed: Remove some more really old files that we don't need.

--- docs/schema.sql
+++
@@ -1,6 +0,0 @@
-# --------------------------------------------------------
-#
-# This script has been supersceeded by the Beehive installer
-# which you will find located at /forum/install/install.php
-#
-# --------------------------------------------------------
